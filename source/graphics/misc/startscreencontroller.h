a2106df56550749c5ec7562a72c0d79155881b3b
Christian Heinemann <heinemann.christian@gmail.com>
2016-10-23 14:39:45 +0200

    scaling of start logo

--- source/graphics/misc/startscreencontroller.h
+++ source/graphics/misc/startscreencontroller.h
@@ -24,7 +24,7 @@ private slots:
     void timeout ();
 
 private:
-    void decreaseOpacityOfLogo ();
+    void scaleAndDecreaseOpacityOfLogo ();
     bool isLogoTransparent () const;
     void restoreScene ();
 
