4a03381447ba19f9f67aa89c48c9381f9053925f
Christian Heinemann <heinemann.christian@gmail.com>
2016-11-23 21:23:17 +0100

    integration test for detection of determinism bug completed

--- source/tests/testsettings.h
+++ source/tests/testsettings.h
@@ -6,7 +6,8 @@
 
 const qreal TEST_REAL_PRECISION = 1.0e-5;
 const qreal TEST_LOW_REAL_PRECISION = 1.0e-1;
-const QString TESTDATA_DETERMINISM_FOLDER = "../source/testdata";
-const int TIMESTEPS = 2000;
+const QString TESTDATA_COMPARISON_REF_FOLDER = "../source/testdata";
+const QString TESTDATA_COMPARISON_SIM_FOLDER = "../source/testdata";
+const int TIMESTEPS = 1000;
 
 #endif // TESTSETTINGS_H
