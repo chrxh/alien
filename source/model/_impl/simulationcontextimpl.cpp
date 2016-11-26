ad96842de6f4010f9f1c9ea9d72cc3d44827e514
Christian Heinemann <heinemann.christian@gmail.com>
2016-11-26 11:15:40 +0100

    SimulationContext added

---
+++ source/model/_impl/simulationcontextimpl.cpp
@@ -0,0 +1,6 @@
+#include "simulationcontextimpl.h"
+
+SimulationContextImpl::SimulationContextImpl()
+{
+
+}
---
+++ source/model/_impl/simulationcontextimpl.h
@@ -0,0 +1,11 @@
+#ifndef SIMULATIONCONTEXTIMPL_H
+#define SIMULATIONCONTEXTIMPL_H
+
+
+class SimulationContextImpl : public SimulationContext
+{
+public:
+    SimulationContextImpl();
+};
+
+#endif // SIMULATIONCONTEXTIMPL_H
