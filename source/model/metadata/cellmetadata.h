e1a1b36f51c17a99772f0b5d31805c6d0814d863
Christian Heinemann <heinemann.christian@gmail.com>
2017-01-11 19:02:48 +0100

    metadata added

---
+++ source/model/metadata/cellmetadata.h
@@ -0,0 +1,14 @@
+#ifndef CELLMETADATA_H
+#define CELLMETADATA_H
+
+#include "model/definitions.h"
+
+struct CellMetadata
+{
+    QString computerSourcecode;
+    QString name;
+    QString description;
+    quint8 color;
+};
+
+#endif // CELLMETADATA_H
