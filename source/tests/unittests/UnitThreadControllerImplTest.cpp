ec77da933ce6f2fb3704cd3389a2d53fbdb21a38
Christian Heinemann <heinemann.christian@gmail.com>
2017-05-04 10:23:59 +0200

    UnitThreadControllerImplTest extended; MultithreadingTest implemented

--- source/tests/unittests/UnitThreadControllerImplTest.cpp
+++ source/tests/unittests/UnitThreadControllerImplTest.cpp
@@ -13,7 +13,8 @@
 
 #include "tests/Predicates.h"
 
-class UnitThreadControllerImplTest : public ::testing::Test
+class UnitThreadControllerImplTest
+	: public ::testing::Test
 {
 public:
 	UnitThreadControllerImplTest();
@@ -25,6 +26,10 @@ protected:
 	UnitThreadControllerImpl* _threadController = nullptr;
 	const IntVector2D _gridSize{ 9, 6 };
 	const IntVector2D _universeSize{ 900, 600 };
+	const std::string msgWrongNumWorkingUnits = "Wrong number of working units.";
+	const std::string msgWrongNumFinishedUnits = "Wrong number of units is finished.";
+	const std::string msgWrongWorkingUnit = "Wrong unit is working.";
+	const std::string msgWrongReadyUnit = "Wrong unit is ready.";
 };
 
 UnitThreadControllerImplTest::UnitThreadControllerImplTest()
@@ -60,18 +65,55 @@ TEST_F(UnitThreadControllerImplTest, testStates)
 		}
 		++index;
 	}
-	ASSERT_EQ(6, indexOfWorkingThreads.size()) << "Wrong number of working threads.";
-	auto const msgWrongThread = "Wrong thread is working";
-	ASSERT_EQ(0, indexOfWorkingThreads[0]) << msgWrongThread;
-	ASSERT_EQ(3, indexOfWorkingThreads[1]) << msgWrongThread;
-	ASSERT_EQ(18, indexOfWorkingThreads[2]) << msgWrongThread;
-	ASSERT_EQ(21, indexOfWorkingThreads[3]) << msgWrongThread;
-	ASSERT_EQ(36, indexOfWorkingThreads[4]) << msgWrongThread;
-	ASSERT_EQ(39, indexOfWorkingThreads[5]) << msgWrongThread;
+	ASSERT_EQ(6, indexOfWorkingThreads.size()) << msgWrongNumWorkingUnits;
+	ASSERT_EQ(0, indexOfWorkingThreads[0]) << msgWrongWorkingUnit;
+	ASSERT_EQ(3, indexOfWorkingThreads[1]) << msgWrongWorkingUnit;
+	ASSERT_EQ(18, indexOfWorkingThreads[2]) << msgWrongWorkingUnit;
+	ASSERT_EQ(21, indexOfWorkingThreads[3]) << msgWrongWorkingUnit;
+	ASSERT_EQ(36, indexOfWorkingThreads[4]) << msgWrongWorkingUnit;
+	ASSERT_EQ(39, indexOfWorkingThreads[5]) << msgWrongWorkingUnit;
 
 	for (auto const& threadAndCalcSignal : _threadController->_threadsAndCalcSignals) {
 		ASSERT_FALSE(threadAndCalcSignal.thr->isReady());
 		ASSERT_FALSE(threadAndCalcSignal.thr->isFinished());
 	}
 }
+
+TEST_F(UnitThreadControllerImplTest, testStatesWithFinished)
+{
+	_threadController->updateDependencies();
+
+	_threadController->_threadsAndCalcSignals[3].thr->setState(UnitThread::State::Finished);
+	_threadController->_threadsAndCalcSignals[24].thr->setState(UnitThread::State::Finished);
+
+	std::vector<int> indexOfWorkingThreads;
+	int index = 0;
+	for (auto const& threadAndCalcSignal : _threadController->_threadsAndCalcSignals) {
+		if (threadAndCalcSignal.thr->isReady()) {
+			threadAndCalcSignal.thr->setState(UnitThread::State::Working);
+			indexOfWorkingThreads.push_back(index);
+		}
+		++index;
+	}
+	ASSERT_EQ(6, indexOfWorkingThreads.size()) << msgWrongNumWorkingUnits;
+	ASSERT_EQ(0, indexOfWorkingThreads[0]) << msgWrongWorkingUnit;
+	ASSERT_EQ(9, indexOfWorkingThreads[1]) << msgWrongWorkingUnit;
+	ASSERT_EQ(18, indexOfWorkingThreads[2]) << msgWrongWorkingUnit;
+	ASSERT_EQ(27, indexOfWorkingThreads[3]) << msgWrongWorkingUnit;
+	ASSERT_EQ(36, indexOfWorkingThreads[4]) << msgWrongWorkingUnit;
+	ASSERT_EQ(45, indexOfWorkingThreads[5]) << msgWrongWorkingUnit;
+
+	for (auto const& threadAndCalcSignal : _threadController->_threadsAndCalcSignals) {
+		ASSERT_FALSE(threadAndCalcSignal.thr->isReady()) << msgWrongReadyUnit;
+	}
+
+	int numFinished = 0;
+	for (auto const& threadAndCalcSignal : _threadController->_threadsAndCalcSignals) {
+		if (threadAndCalcSignal.thr->isFinished()) {
+			++numFinished;
+		}
+	}
+	ASSERT_EQ(2, numFinished) << msgWrongNumFinishedUnits;
+}
+
 
