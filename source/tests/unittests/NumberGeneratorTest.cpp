6067c41edaf733eedcb8cd5101c48ed6b314fc45
Christian Heinemann <heinemann.christian@gmail.com>
2017-05-07 13:39:46 +0200

    NumberGeneratorTest added

---
+++ source/tests/unittests/NumberGeneratorTest.cpp
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+
+#include "global/ServiceLocator.h"
+#include "global/GlobalFactory.h"
+#include "global/NumberGenerator.h"
+
+class NumberGeneratorTest : public ::testing::Test
+{
+public:
+	NumberGeneratorTest();
+	~NumberGeneratorTest();
+
+protected:
+	NumberGenerator* _numberGen = nullptr;
+};
+
+NumberGeneratorTest::NumberGeneratorTest()
+{
+	GlobalFactory* factory = ServiceLocator::getInstance().getService<GlobalFactory>();
+	_numberGen = factory->buildRandomNumberGenerator();
+}
+
+NumberGeneratorTest::~NumberGeneratorTest()
+{
+	delete _numberGen;
+}
+
+
+TEST_F(NumberGeneratorTest, testTags)
+{
+	_numberGen->init(123, 1);
+	quint64 tag = _numberGen->getTag();
+	ASSERT_EQ(1, tag >> 48);
+	ASSERT_EQ(0, tag & 0xffffffffffff);
+	tag = _numberGen->getTag();
+	ASSERT_EQ(1, tag >> 48);
+	ASSERT_EQ(1, tag & 0xffffffffffff);
+	tag = _numberGen->getTag();
+	ASSERT_EQ(1, tag >> 48);
+	ASSERT_EQ(2, tag & 0xffffffffffff);
+
+	_numberGen->init(123, 23);
+	tag = _numberGen->getTag();
+	ASSERT_EQ(23, tag >> 48);
+	ASSERT_EQ(0, tag & 0xffffffffffff);
+	tag = _numberGen->getTag();
+	ASSERT_EQ(23, tag >> 48);
+	ASSERT_EQ(1, tag & 0xffffffffffff);
+}
+
