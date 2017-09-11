519e8dc93742732dd9c1acf43cf98a5a2e3fe57c
Christian Heinemann <heinemann.christian@gmail.com>
2017-09-11 19:59:50 +0200

    DataDescriptionTransferTest refactored

--- source/Model/Entities/Descriptions.cpp
+++ source/Model/Entities/Descriptions.cpp
@@ -2,79 +2,10 @@
 #include "ChangeDescriptions.h"
 #include "Model/Settings.h"
 
-namespace
-{
-	template<typename T>
-	bool isCompatible(T const& a, T const& b)
-	{
-		return a == b;
-	}
-
-	template<>
-	bool isCompatible<QVector2D>(QVector2D const& vec1, QVector2D const& vec2)
-	{
-		return std::abs(vec1.x() - vec2.x()) < ALIEN_PRECISION
-			&& std::abs(vec1.y() - vec2.y()) < ALIEN_PRECISION;
-	}
-
-	template<>
-	bool isCompatible<double>(double const& a, double const& b)
-	{
-		return std::abs(a - b) < ALIEN_PRECISION;
-	}
-
-	template<>
-	bool isCompatible<CellDescription>(CellDescription const& a, CellDescription const& b)
-	{
-		return a.isCompatibleWith(b);
-	}
-
-	template<>
-	bool isCompatible<ClusterDescription>(ClusterDescription const& a, ClusterDescription const& b)
-	{
-		return a.isCompatibleWith(b);
-	}
-
-	template<>
-	bool isCompatible<ParticleDescription>(ParticleDescription const& a, ParticleDescription const& b)
-	{
-		return a.isCompatibleWith(b);
-	}
-
-	template<typename T>
-	bool isCompatible(optional<T> const& a, optional<T> const& b)
-	{
-		if (!a || !b) {
-			return true;
-		}
-		return isCompatible(*a, *b);
-	}
-
-	template<typename T>
-	bool isCompatible(vector<T> const& a, vector<T> const& b)
-	{
-		if (a.size() != b.size()) {
-			false;
-		}
-		for (int i = 0; i < a.size(); ++i) {
-			if (!isCompatible(a.at(i), b.at(i))) {
-				return false;
-			}
-		}
-		return true;
-	}
-}
-
 
 bool TokenDescription::operator==(TokenDescription const& other) const {
 	return energy == other.energy
 		&& data == other.data;
-}
-
-bool TokenDescription::isCompatibleWith(TokenDescription const & other) const
-{
-	return isCompatible(energy, other.energy)
-		&& isCompatible(data, other.data);
 }
 
 CellDescription::CellDescription(CellChangeDescription const & change)
@@ -89,20 +20,6 @@ CellDescription::CellDescription(CellChangeDescription const & change)
 	metadata = change.metadata;
 	cellFunction = change.cellFunction;
 	tokens = change.tokens;
-}
-
-bool CellDescription::isCompatibleWith(CellChangeDescription const & other) const
-{
-	return isCompatible(pos, other.pos)
-		&& isCompatible(energy, other.energy)
-		&& isCompatible(maxConnections, other.maxConnections)
-		&& isCompatible(connectingCells, other.connectingCells)
-		&& isCompatible(tokenBlocked, other.tokenBlocked)
-		&& isCompatible(tokenBranchNumber, other.tokenBranchNumber)
-		&& isCompatible(metadata, other.metadata)
-		&& isCompatible(cellFunction, other.cellFunction)
-		&& isCompatible(tokens, other.tokens)
-		;
 }
 
 ClusterDescription::ClusterDescription(ClusterChangeDescription const & change)
@@ -124,16 +41,6 @@ ClusterDescription::ClusterDescription(ClusterChangeDescription const & change)
 
 }
 
-bool ClusterDescription::isCompatibleWith(ClusterDescription const & other) const
-{
-	return isCompatible(pos, other.pos)
-		&& isCompatible(vel, other.vel)
-		&& isCompatible(angle, other.angle) 
-		&& isCompatible(angularVel, other.angularVel)
-		&& isCompatible(metadata, other.metadata)
-		&& isCompatible(cells, other.cells);
-}
-
 ParticleDescription::ParticleDescription(ParticleChangeDescription const & change)
 {
 	id = change.id;
@@ -141,18 +48,4 @@ ParticleDescription::ParticleDescription(ParticleChangeDescription const & chang
 	vel = change.vel;
 	energy = change.energy;
 	metadata = change.metadata;
-}
-
-bool ParticleDescription::isCompatibleWith(ParticleDescription const & other) const
-{
-	return isCompatible(pos, other.pos)
-		&& isCompatible(vel, other.vel)
-		&& isCompatible(energy, other.energy)
-		&& isCompatible(metadata, other.metadata);
-}
-
-bool DataDescription::isCompatibleWith(DataDescription const & other) const
-{
-	return isCompatible(clusters, other.clusters)
-		&& isCompatible(particles, other.particles);
 }
