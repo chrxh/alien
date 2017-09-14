ca33c18bbf33d4422e6061d7c14c8e4dcca220d5
Christian Heinemann <heinemann.christian@gmail.com>
2017-09-14 20:54:10 +0200

    test for adding and deleting cell clusters added

--- source/Tests/integrationtests/DataDescriptionTransferTest.cpp
+++ source/Tests/integrationtests/DataDescriptionTransferTest.cpp
@@ -21,30 +21,61 @@ public:
 	~DataDescriptionTransferTest();
 
 protected:
+	ClusterDescription createClusterDescription(int numCells) const;
+
+	ModelBuilderFacade* _facade = nullptr;
 	SimulationController* _controller = nullptr;
 	SimulationContext* _context = nullptr;
 	SimulationParameters* _parameters = nullptr;
 	NumberGenerator* _numberGen = nullptr;
+	SimulationAccess* _access = nullptr;
 	IntVector2D _gridSize{ 6, 6 };
 	IntVector2D _universeSize{ 600, 300 };
 };
 
 DataDescriptionTransferTest::DataDescriptionTransferTest()
 {
-	ModelBuilderFacade* facade = ServiceLocator::getInstance().getService<ModelBuilderFacade>();
+	_facade = ServiceLocator::getInstance().getService<ModelBuilderFacade>();
 	GlobalFactory* factory = ServiceLocator::getInstance().getService<GlobalFactory>();
-	auto symbols = facade->buildDefaultSymbolTable();
-	_parameters = facade->buildDefaultSimulationParameters();
-	_controller = facade->buildSimulationController(1, _gridSize, _universeSize, symbols, _parameters);
+	auto symbols = _facade->buildDefaultSymbolTable();
+	_parameters = _facade->buildDefaultSimulationParameters();
+	_controller = _facade->buildSimulationController(1, _gridSize, _universeSize, symbols, _parameters);
 	_context = static_cast<SimulationContext*>(_controller->getContext());
 	_numberGen = factory->buildRandomNumberGenerator();
 	_numberGen->init(123123, 0);
+
+	_access = _facade->buildSimulationAccess(_context);
 }
 
 DataDescriptionTransferTest::~DataDescriptionTransferTest()
 {
+	delete _access;
 	delete _controller;
 	delete _numberGen;
+}
+
+ClusterDescription DataDescriptionTransferTest::createClusterDescription(int numCells) const
+{
+	ClusterDescription cluster;
+	QVector2D pos(_numberGen->getRandomReal(0, 499), _numberGen->getRandomReal(0, 299));
+	cluster.setId(_numberGen->getTag()).setPos(pos).setVel(QVector2D(_numberGen->getRandomReal(-1, 1), _numberGen->getRandomReal(-1, 1)));
+	for (int j = 0; j < numCells; ++j) {
+		cluster.addCell(
+			CellDescription().setEnergy(_parameters->cellCreationEnergy).setPos(pos + QVector2D(-static_cast<float>(numCells - 1) / 2.0 + j, 0))
+			.setMaxConnections(2).setId(_numberGen->getTag())
+		);
+	}
+	for (int j = 0; j < numCells; ++j) {
+		list<uint64_t> connectingCells;
+		if (j > 0) {
+			connectingCells.emplace_back(cluster.cells->at(j - 1).id);
+		}
+		if (j < numCells - 1) {
+			connectingCells.emplace_back(cluster.cells->at(j + 1).id);
+		}
+		cluster.cells->at(j).setConnectingCells(connectingCells);
+	}
+	return cluster;
 }
 
 namespace
@@ -141,42 +172,21 @@ namespace
 	}
 }
 
-TEST_F(DataDescriptionTransferTest, testTransferRandomData)
+TEST_F(DataDescriptionTransferTest, testAddRandomData)
 {
-	ModelBuilderFacade* facade = ServiceLocator::getInstance().getService<ModelBuilderFacade>();
-	auto access = facade->buildSimulationAccess(_context);
 
 	DataDescription dataBefore;
 	int numClusters = 100;
 	for (int i = 1; i <= numClusters; ++i) {
 		QVector2D pos(_numberGen->getRandomReal(0, 499), _numberGen->getRandomReal(0, 299));
-		int numCells = i;
-		ClusterDescription cluster;
-		cluster.setPos(pos).setVel(QVector2D(_numberGen->getRandomReal(-1, 1), _numberGen->getRandomReal(-1, 1)));
-		for (int j = 0; j < numCells; ++j) {
-			cluster.addCell(
-				CellDescription().setEnergy(_parameters->cellCreationEnergy).setPos(pos + QVector2D(-static_cast<float>(numCells - 1) / 2.0 + j, 0))
-					.setMaxConnections(2).setId(_numberGen->getTag())
-			);
-		}
-		for (int j = 0; j < numCells; ++j) {
-			list<uint64_t> connectingCells;
-			if (j > 0) {
-				connectingCells.emplace_back(cluster.cells->at(j - 1).id);
-			}
-			if (j < numCells - 1) {
-				connectingCells.emplace_back(cluster.cells->at(j + 1).id);
-			}
-			cluster.cells->at(j).setConnectingCells(connectingCells);
-		}
-		dataBefore.addCluster(cluster);
+		dataBefore.addCluster(createClusterDescription(i));
 	}
-	access->updateData(dataBefore);
-	IntRect rect = { { 0, 0 }, { _universeSize.x - 1, _universeSize.y - 1 } };
+	_access->updateData(dataBefore);
 
+	IntRect rect = { { 0, 0 }, { _universeSize.x - 1, _universeSize.y - 1 } };
 	ResolveDescription resolveDesc;
-	access->requireData(rect, resolveDesc);
-	DataDescription dataAfter = access->retrieveData();
+	_access->requireData(rect, resolveDesc);
+	DataDescription dataAfter = _access->retrieveData();
 
 	ASSERT_TRUE(dataBefore.clusters->size() == dataAfter.clusters->size());
 	std::sort(dataBefore.clusters->begin(), dataBefore.clusters->end(), [](auto const &cluster1, auto const &cluster2) {
@@ -185,7 +195,7 @@ TEST_F(DataDescriptionTransferTest, testTransferRandomData)
 	std::sort(dataAfter.clusters->begin(), dataAfter.clusters->end(), [](auto const &cluster1, auto const &cluster2) {
 		return cluster1.cells->size() <= cluster2.cells->size();
 	});
-	for (int i = 0; i < numClusters; ++i) {
+	for (int i = 0; i < dataAfter.clusters->size(); ++i) {
 		auto& cluster1 = dataAfter.clusters->at(i);
 		auto& cluster2 = dataBefore.clusters->at(i);
 		std::sort(cluster1.cells->begin(), cluster1.cells->end(), [](auto const &cell1, auto const &cell2) {
@@ -196,6 +206,50 @@ TEST_F(DataDescriptionTransferTest, testTransferRandomData)
 		});
 	}
 	ASSERT_TRUE(isCompatible(dataBefore, dataAfter));
-	delete access;
 }
 
+TEST_F(DataDescriptionTransferTest, testAddAndDeleteRandomData)
+{
+
+	DataDescription dataBefore;
+	int numClusters = 100;
+	for (int i = 1; i <= numClusters; ++i) {
+		QVector2D pos(_numberGen->getRandomReal(0, 499), _numberGen->getRandomReal(0, 299));
+		dataBefore.addCluster(createClusterDescription(i));
+	}
+	_access->updateData(dataBefore);
+
+	DataChangeDescription dataChange;
+	for (int i = 0; i <= 49; ++i) {
+		uint64_t id = dataBefore.clusters->at(i).id;
+		auto pos = *dataBefore.clusters->at(i).pos;
+		dataChange.addDeletedCluster(ClusterChangeDescription().setId(id).setPos(pos));
+	}
+	_access->updateData(dataChange);
+
+	dataBefore.clusters->erase(dataBefore.clusters->begin(), dataBefore.clusters->begin() + 50);
+
+	IntRect rect = { { 0, 0 },{ _universeSize.x - 1, _universeSize.y - 1 } };
+	ResolveDescription resolveDesc;
+	_access->requireData(rect, resolveDesc);
+	DataDescription dataAfter = _access->retrieveData();
+
+	ASSERT_TRUE(dataBefore.clusters->size() == dataAfter.clusters->size());
+	std::sort(dataBefore.clusters->begin(), dataBefore.clusters->end(), [](auto const &cluster1, auto const &cluster2) {
+		return cluster1.cells->size() <= cluster2.cells->size();
+	});
+	std::sort(dataAfter.clusters->begin(), dataAfter.clusters->end(), [](auto const &cluster1, auto const &cluster2) {
+		return cluster1.cells->size() <= cluster2.cells->size();
+	});
+	for (int i = 0; i < dataAfter.clusters->size(); ++i) {
+		auto& cluster1 = dataAfter.clusters->at(i);
+		auto& cluster2 = dataBefore.clusters->at(i);
+		std::sort(cluster1.cells->begin(), cluster1.cells->end(), [](auto const &cell1, auto const &cell2) {
+			return cell1.pos->x() <= cell2.pos->x();
+		});
+		std::sort(cluster2.cells->begin(), cluster2.cells->end(), [](auto const &cell1, auto const &cell2) {
+			return cell1.pos->x() <= cell2.pos->x();
+		});
+	}
+	ASSERT_TRUE(isCompatible(dataBefore, dataAfter));
+}
