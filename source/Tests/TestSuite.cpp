9f8d397c9c071b173c3d326624008659626cc4ef
Christian Heinemann <heinemann.christian@gmail.com>
2018-10-13 18:22:46 +0200

    services registered => all tests succeed

--- source/Tests/TestSuite.cpp
+++ source/Tests/TestSuite.cpp
@@ -1,10 +1,12 @@
 #include <gtest/gtest.h>
 #include <QApplication>
 
+#include "ModelBasic/ModelBasicServices.h"
 #include "ModelCpu/ModelCpuServices.h"
 
 int main(int argc, char** argv) {
-	ModelCpuServices _modelServices;
+	ModelBasicServices _modelBasicServices;
+	ModelCpuServices _modelCpuServices;
 
     QApplication app(argc, argv);
 
