1e945a9bcb73fce8ca59df7fab5685978fcc68a7
Christian Heinemann <heinemann.christian@gmail.com>
2019-06-09 14:44:29 +0200

    scanner function almost implemented

--- source/ModelGpu/Base.cuh
+++ source/ModelGpu/Base.cuh
@@ -172,6 +172,60 @@ public:
 	}
 };
 
+template<typename T>
+struct HashFunctor
+{
+};
+
+
+template<typename T>
+struct HashFunctor<T*>
+{
+    __device__ __inline__ int operator()(T* const& element)
+    {
+        return reinterpret_cast<std::uintptr_t>(element) * 17;
+    }
+};
+
+
+template<typename T, typename Hash = HashFunctor<T>>
+class HashSet
+{
+public:
+    __device__ __inline__ HashSet(int size, T* data)
+        : _size(size), _data(data)
+    {
+        for (int i = 0; i < size; ++i) {
+            _data[i] = nullptr;
+        }
+    }
+
+    __device__ __inline__ void insert(T const& element)
+    {
+        int index = _hash(element) % _size;
+        while (_data[index]) {
+            index = (++index) % _size;
+        }
+        _data[index] = element;
+    }
+
+    __device__ __inline__ bool contains(T const& element)
+    {
+        int index = _hash(element) % _size;
+        for (int i = 0; i < _size; ++i, index = (++index) % _size) {
+            if (_data[index] == element) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+private:
+    T* _data;
+    int _size;
+    Hash _hash;
+};
+
 __device__ __inline__ void calcPartition(int numEntities, int division, int numDivisions, int& startIndex, int& endIndex)
 {
 	int entitiesByDivisions = numEntities / numDivisions;
