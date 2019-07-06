4e0c5df57db2289c0f4b42b4134f6d78e083a7e2
Christian Heinemann <heinemann.christian@gmail.com>
2019-07-06 23:20:46 +0200

    - further scan tests added
    - bug in angle calculation fixed

--- source/Tests/ScannerGpuTests.cpp
+++ source/Tests/ScannerGpuTests.cpp
@@ -15,7 +15,11 @@ public:
 protected:
     virtual void SetUp();
 
-    void checkedScannedCellWithToken(CellDescription const& cell, TokenDescription const& token) const;
+    void checkScannedCellWithToken(
+        CellDescription const& cell,
+        CellDescription const& prevCell,
+        CellDescription const& prevPrevCell,
+        TokenDescription const& token) const;
 };
 
 
@@ -29,12 +33,29 @@ void ScannerGpuTests::SetUp()
     _context->setSimulationParameters(_parameters);
 }
 
-void ScannerGpuTests::checkedScannedCellWithToken(CellDescription const& cell, TokenDescription const& token) const
+void ScannerGpuTests::checkScannedCellWithToken(
+    CellDescription const& cell,
+    CellDescription const& prevCell,
+    CellDescription const& prevPrevCell,
+    TokenDescription const& token) const
 {
     EXPECT_EQ(*cell.maxConnections, token.data->at(Enums::Scanner::OUT_CELL_MAX_CONNECTIONS));
     EXPECT_EQ(*cell.tokenBranchNumber, token.data->at(Enums::Scanner::OUT_CELL_BRANCH_NO));
     EXPECT_EQ(cell.cellFeature->type, token.data->at(Enums::Scanner::OUT_CELL_FUNCTION));
     EXPECT_EQ(static_cast<int>(*cell.energy), token.data->at(Enums::Scanner::OUT_ENERGY));
+    EXPECT_EQ(
+        QuantityConverter::convertShiftLenToData((*cell.pos - *prevCell.pos).length()),
+        token.data->at(Enums::Scanner::OUT_DISTANCE));
+
+    if (prevCell.pos != prevPrevCell.pos) {
+        auto a1 = Physics::angleOfVector(*prevPrevCell.pos - *prevCell.pos);
+        auto a2 = Physics::angleOfVector(*prevCell.pos - *cell.pos);
+        auto angle = a1 - a2;
+        EXPECT_EQ(QuantityConverter::convertAngleToData(angle), token.data->at(Enums::Scanner::OUT_ANGLE));
+    }
+    else {
+        EXPECT_EQ(0, token.data->at(Enums::Scanner::OUT_ANGLE));
+    }
 }
 
 TEST_F(ScannerGpuTests, testScanOriginCell)
@@ -63,7 +84,7 @@ TEST_F(ScannerGpuTests, testScanOriginCell)
     auto const& newSecondCell = cellByCellId.at(secondCell.id);
     auto const& newToken = newSecondCell.tokens->at(0);
 
-    checkedScannedCellWithToken(newSecondCell, newToken);
+    checkScannedCellWithToken(newSecondCell, newSecondCell, newSecondCell, newToken);
     EXPECT_EQ(Enums::ScannerOut::SUCCESS, newToken.data->at(Enums::Scanner::OUT));
     EXPECT_EQ(0, newToken.data->at(Enums::Scanner::OUT_DISTANCE));
     EXPECT_EQ(1, newToken.data->at(Enums::Scanner::INOUT_CELL_NUMBER));
@@ -103,11 +124,8 @@ TEST_F(ScannerGpuTests, testScanSecondCell)
     auto const& newTokenSourceCell = cellByCellId.at(tokenSourceCell.id);
     auto const& newToken = newCell.tokens->at(0);
 
-    checkedScannedCellWithToken(newTokenSourceCell, newToken);
+    checkScannedCellWithToken(newTokenSourceCell, newCell, newCell, newToken);
     EXPECT_EQ(Enums::ScannerOut::SUCCESS, newToken.data->at(Enums::Scanner::OUT));
-    EXPECT_EQ(
-        QuantityConverter::convertShiftLenToData((*tokenSourceCell.pos - *middleCell.pos).length()),
-        newToken.data->at(Enums::Scanner::OUT_DISTANCE));
     EXPECT_EQ(2, newToken.data->at(Enums::Scanner::INOUT_CELL_NUMBER));
     EXPECT_EQ(25, newToken.data->at(Enums::Scanner::OUT_MASS));
 }
@@ -147,13 +165,11 @@ TEST_F(ScannerGpuTests, testScanThirdCell)
     auto const& cellByCellId = IntegrationTestHelper::getCellByCellId(newData);
     auto const& newMiddleCell = cellByCellId.at(middleCell.id);
     auto const& newScanCell = cellByCellId.at(scanCell.id);
+    auto const& newTokenSourceCell = cellByCellId.at(tokenSourceCell.id);
     auto const& newToken = newMiddleCell.tokens->at(0);
 
-    checkedScannedCellWithToken(newScanCell, newToken);
+    checkScannedCellWithToken(newScanCell, newTokenSourceCell, newMiddleCell, newToken);
     EXPECT_EQ(Enums::ScannerOut::SUCCESS, newToken.data->at(Enums::Scanner::OUT));
-    EXPECT_EQ(
-        QuantityConverter::convertShiftLenToData((*scanCell.pos - *tokenSourceCell.pos).length()),
-        newToken.data->at(Enums::Scanner::OUT_DISTANCE));
     EXPECT_EQ(3, newToken.data->at(Enums::Scanner::INOUT_CELL_NUMBER));
     EXPECT_EQ(25, newToken.data->at(Enums::Scanner::OUT_MASS));
 }
@@ -186,6 +202,7 @@ TEST_F(ScannerGpuTests, testScanDistantCell)
     scanCell.energy = _parameters.cellMinEnergy + 10.5f;
 
     auto& prevScanCell = cluster.cells->at(6);
+    auto& prevPrevScanCell = cluster.cells->at(7);
 
     origData.addCluster(cluster);
 
@@ -196,13 +213,137 @@ TEST_F(ScannerGpuTests, testScanDistantCell)
     auto const& cellByCellId = IntegrationTestHelper::getCellByCellId(newData);
     auto const& newMiddleCell = cellByCellId.at(middleCell.id);
     auto const& newScanCell = cellByCellId.at(scanCell.id);
+    auto const& newPrevScanCell = cellByCellId.at(prevScanCell.id);
+    auto const& newPrevPrevScanCell = cellByCellId.at(prevPrevScanCell.id);
     auto const& newToken = newMiddleCell.tokens->at(0);
 
-    checkedScannedCellWithToken(newScanCell, newToken);
+    checkScannedCellWithToken(newScanCell, newPrevScanCell, newPrevPrevScanCell, newToken);
     EXPECT_EQ(Enums::ScannerOut::SUCCESS, newToken.data->at(Enums::Scanner::OUT));
-    EXPECT_EQ(
-        QuantityConverter::convertShiftLenToData((*scanCell.pos - *prevScanCell.pos).length()),
-        newToken.data->at(Enums::Scanner::OUT_DISTANCE));
     EXPECT_EQ(10, newToken.data->at(Enums::Scanner::INOUT_CELL_NUMBER));
+    EXPECT_EQ(25, newToken.data->at(Enums::Scanner::OUT_MASS));
+}
+
+/**
+* Situation: - 5x5 cluster with scanner function at middle cell
+*            - token coming from left
+*            - scanning cell number is 24
+* Expected result: correct cell (at (1, 1)-position of the cluster) should be scanned
+*/
+TEST_F(ScannerGpuTests, testScanFinished)
+{
+    DataDescription origData;
+    auto cluster = createRectangularCluster({ 5, 5 }, QVector2D{}, QVector2D{});
+
+    auto& tokenSourceCell = cluster.cells->at(11);
+    tokenSourceCell.tokenBranchNumber = 0;
+    auto token = createSimpleToken();
+    (*token.data)[Enums::Scanner::INOUT_CELL_NUMBER] = 24;
+    tokenSourceCell.addToken(token);
+
+    auto& middleCell = cluster.cells->at(12);
+    middleCell.tokenBranchNumber = 1;
+    middleCell.cellFeature = CellFeatureDescription().setType(Enums::CellFunction::SCANNER);
+
+    auto& scanCell = cluster.cells->at(0);
+    scanCell.cellFeature = CellFeatureDescription().setType(Enums::CellFunction::CONSTRUCTOR);
+    scanCell.tokenBranchNumber = 2;
+    scanCell.energy = _parameters.cellMinEnergy + 10.5f;
+
+    auto& prevScanCell = cluster.cells->at(1);
+    auto& prevPrevScanCell = cluster.cells->at(2);
+
+    origData.addCluster(cluster);
+
+    IntegrationTestHelper::updateData(_access, origData);
+    IntegrationTestHelper::runSimulation(1, _controller);
+
+    DataDescription newData = IntegrationTestHelper::getContent(_access, { { 0, 0 },{ _universeSize.x, _universeSize.y } });
+    auto const& cellByCellId = IntegrationTestHelper::getCellByCellId(newData);
+    auto const& newMiddleCell = cellByCellId.at(middleCell.id);
+    auto const& newScanCell = cellByCellId.at(scanCell.id);
+    auto const& newPrevScanCell = cellByCellId.at(prevScanCell.id);
+    auto const& newPrevPrevScanCell = cellByCellId.at(prevPrevScanCell.id);
+    auto const& newToken = newMiddleCell.tokens->at(0);
+
+    checkScannedCellWithToken(newScanCell, newPrevScanCell, newPrevPrevScanCell, newToken);
+    EXPECT_EQ(Enums::ScannerOut::FINISHED, newToken.data->at(Enums::Scanner::OUT));
+    EXPECT_EQ(25, newToken.data->at(Enums::Scanner::INOUT_CELL_NUMBER));
+    EXPECT_EQ(25, newToken.data->at(Enums::Scanner::OUT_MASS));
+}
+
+/**
+* Situation: - 5x5 cluster with scanner function at middle cell
+*            - token coming from left
+*            - scanning cell number is 25
+* Expected result: correct cell (at (3, 3)-position of the cluster) should be scanned
+*/
+TEST_F(ScannerGpuTests, testScanRestart1)
+{
+    DataDescription origData;
+    auto cluster = createRectangularCluster({ 5, 5 }, QVector2D{}, QVector2D{});
+
+    auto& tokenSourceCell = cluster.cells->at(11);
+    tokenSourceCell.tokenBranchNumber = 0;
+    auto token = createSimpleToken();
+    (*token.data)[Enums::Scanner::INOUT_CELL_NUMBER] = 25;
+    tokenSourceCell.addToken(token);
+
+    auto& middleCell = cluster.cells->at(12);
+    middleCell.tokenBranchNumber = 1;
+    middleCell.cellFeature = CellFeatureDescription().setType(Enums::CellFunction::SCANNER);
+    middleCell.energy = _parameters.cellMinEnergy + 10.5f;
+
+
+    origData.addCluster(cluster);
+
+    IntegrationTestHelper::updateData(_access, origData);
+    IntegrationTestHelper::runSimulation(1, _controller);
+
+    DataDescription newData = IntegrationTestHelper::getContent(_access, { { 0, 0 },{ _universeSize.x, _universeSize.y } });
+    auto const& cellByCellId = IntegrationTestHelper::getCellByCellId(newData);
+    auto const& newMiddleCell = cellByCellId.at(middleCell.id);
+    auto const& newToken = newMiddleCell.tokens->at(0);
+
+    checkScannedCellWithToken(newMiddleCell, newMiddleCell, newMiddleCell, newToken);
+    EXPECT_EQ(Enums::ScannerOut::RESTART, newToken.data->at(Enums::Scanner::OUT));
+    EXPECT_EQ(1, newToken.data->at(Enums::Scanner::INOUT_CELL_NUMBER));
+    EXPECT_EQ(25, newToken.data->at(Enums::Scanner::OUT_MASS));
+}
+
+/**
+* Situation: - 5x5 cluster with scanner function at middle cell
+*            - token coming from left
+*            - scanning cell number is 180
+* Expected result: correct cell (at (3, 3)-position of the cluster) should be scanned
+*/
+TEST_F(ScannerGpuTests, testScanRestart2)
+{
+    DataDescription origData;
+    auto cluster = createRectangularCluster({ 5, 5 }, QVector2D{}, QVector2D{});
+
+    auto& tokenSourceCell = cluster.cells->at(11);
+    tokenSourceCell.tokenBranchNumber = 0;
+    auto token = createSimpleToken();
+    (*token.data)[Enums::Scanner::INOUT_CELL_NUMBER] = 180;
+    tokenSourceCell.addToken(token);
+
+    auto& middleCell = cluster.cells->at(12);
+    middleCell.tokenBranchNumber = 1;
+    middleCell.cellFeature = CellFeatureDescription().setType(Enums::CellFunction::SCANNER);
+    middleCell.energy = _parameters.cellMinEnergy + 10.5f;
+
+    origData.addCluster(cluster);
+
+    IntegrationTestHelper::updateData(_access, origData);
+    IntegrationTestHelper::runSimulation(1, _controller);
+
+    DataDescription newData = IntegrationTestHelper::getContent(_access, { { 0, 0 },{ _universeSize.x, _universeSize.y } });
+    auto const& cellByCellId = IntegrationTestHelper::getCellByCellId(newData);
+    auto const& newMiddleCell = cellByCellId.at(middleCell.id);
+    auto const& newToken = newMiddleCell.tokens->at(0);
+
+    checkScannedCellWithToken(newMiddleCell, newMiddleCell, newMiddleCell, newToken);
+    EXPECT_EQ(Enums::ScannerOut::RESTART, newToken.data->at(Enums::Scanner::OUT));
+    EXPECT_EQ(1, newToken.data->at(Enums::Scanner::INOUT_CELL_NUMBER));
     EXPECT_EQ(25, newToken.data->at(Enums::Scanner::OUT_MASS));
 }
