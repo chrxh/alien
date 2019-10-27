4a9e575c0a037b06675bd4eee42a4e71036b866b
Christian Heinemann <heinemann.christian@gmail.com>
2019-10-27 11:56:42 +0100

    lock bug in constructor fixeg

--- source/ModelGpu/Cell.cuh
+++ source/ModelGpu/Cell.cuh
@@ -86,7 +86,24 @@ struct Cell
 
     __device__ __inline__ void releaseLock()
     {
+        __threadfence();
         atomicExch(&locked, 0);
+    }
+
+    __device__ __inline__ void getBlockLock()
+    {
+        while (1 == atomicExch_block(&locked, 1)) {}
+    }
+
+    __device__ __inline__ bool tryBlockLock()
+    {
+        return 0 == atomicExch_block(&locked, 1);
+    }
+
+    __device__ __inline__ void releaseBlockLock()
+    {
+        __threadfence_block();
+        atomicExch_block(&locked, 0);
     }
 
 private:
