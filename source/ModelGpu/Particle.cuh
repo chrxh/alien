4052909f548131251786fc7a759940faed9e51bf
Christian Heinemann <heinemann.christian@gmail.com>
2019-11-16 12:38:58 +0100

    debug code for energy check removed

--- source/ModelGpu/Particle.cuh
+++ source/ModelGpu/Particle.cuh
@@ -14,42 +14,17 @@ struct Particle
 
     __device__ __inline__ float getEnergy()
     {
-        auto const value = atomicAdd(&_energy, 0);
-        if (value < 0) {
-            printf("Particle::getEnergy negative, value: %f, parameter: \n", value);
-            while (true) {}
-        }
-        if (isnan(value)) {
-            printf("Particle::getEnergy nan, value: %f, parameter: \n", value);
-            while (true) {}
-        }
-        return value;
+        return atomicAdd(&_energy, 0);
     }
 
     __device__ __inline__ void setEnergy(float value, int parameter)
     {
-        if (value < 0) {
-            printf("Particle::setEnergy negative, value: %f, parameter: %d\n", value, parameter);
-            while (true) {}
-        }
-        if (isnan(value)) {
-            printf("Particle::setEnergy nan, value: %f, parameter: %d\n", value, parameter);
-            while (true) {}
-        }
         atomicExch(&_energy, value);
     }
 
     __device__ __inline__ void changeEnergy(float changeValue, int parameter = -1)
     {
-        auto const origValue = atomicAdd(&_energy, changeValue);
-        if (getEnergy() < 0) {
-            printf("Particle::changeEnergy negative, before: %f, after: %f, change: %f, parameter: %d\n", origValue, getEnergy(), changeValue, parameter);
-            while (true) {}
-        }
-        if (isnan(getEnergy())) {
-            printf("Particle::changeEnergy nan, before: %f, after: %f, change: %f, parameter: %d\n", origValue, getEnergy(), changeValue, parameter);
-            while (true) {}
-        }
+        atomicAdd(&_energy, changeValue);
     }
 
 
