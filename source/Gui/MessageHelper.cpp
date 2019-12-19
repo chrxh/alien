7036fc253c4489768188ead9000ac9d01ff80d64
Christian Heinemann <heinemann.christian@gmail.com>
2019-12-19 18:03:56 +0100

    event processing for progress dialog triggered without timeout

--- source/Gui/MessageHelper.cpp
+++ source/Gui/MessageHelper.cpp
@@ -9,6 +9,6 @@ QWidget * MessageHelper::getProgress(std::string message, QWidget * parent)
     auto const progress = new QProgressDialog(QString::fromStdString(message), QString(), 0, 0, parent);
     progress->setModal(false);
     progress->show();
-    QCoreApplication::processEvents(QEventLoop::DialogExec, 100);
+    QCoreApplication::processEvents(QEventLoop::DialogExec);
     return progress;
 }
