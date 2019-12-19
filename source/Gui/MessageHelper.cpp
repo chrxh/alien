afb24c791736eb32964a91f328a096519475f526
Christian Heinemann <heinemann.christian@gmail.com>
2019-12-19 18:02:34 +0100

    MessageHelper added + loading message

--- source/Gui/MessageHelper.cpp
+++ source/Gui/MessageHelper.cpp
@@ -1,11 +1,14 @@
 #include <QProgressDialog>
+#include <QCoreApplication>
 
 #include "MessageHelper.h"
 
 QWidget * MessageHelper::getProgress(std::string message, QWidget * parent)
 {
+
     auto const progress = new QProgressDialog(QString::fromStdString(message), QString(), 0, 0, parent);
     progress->setModal(false);
     progress->show();
+    QCoreApplication::processEvents(QEventLoop::DialogExec, 100);
     return progress;
 }
