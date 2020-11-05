7e4341c98981672474bf2150165f3d209c542731
Christian Heinemann <heinemann.christian@gmail.com>
2020-11-05 21:25:15 +0100

    + showing all relevant simulation info in web simulation selection dialog + adding refresh button

--- source/Gui/WebSimulationSelectionView.cpp
+++ source/Gui/WebSimulationSelectionView.cpp
@@ -2,17 +2,27 @@
 
 #include "Gui/Settings.h"
 
+#include "WebSimulationSelectionController.h"
 #include "WebSimulationTableModel.h"
 #include "ui_WebSimulationSelectionView.h"
 
-WebSimulationSelectionView::WebSimulationSelectionView(WebSimulationTableModel* model, QWidget *parent)
+WebSimulationSelectionView::WebSimulationSelectionView(
+    WebSimulationSelectionController* controller, 
+    WebSimulationTableModel* model, 
+    QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::WebSimulationSelectionView)
+    , _controller(controller)
 {
     ui->setupUi(this);
     setFont(GuiSettings::getGlobalFont());
 
     ui->webSimulationTableView->setModel(model);
+    ui->webSimulationTableView->setAlternatingRowColors(true);
+    ui->webSimulationTableView->horizontalHeader()->setStretchLastSection(true);
+    ui->webSimulationTableView->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+
+    connect(ui->refreshButton, &QPushButton::clicked, _controller, &WebSimulationSelectionController::refresh);
 }
 
 
