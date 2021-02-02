0eb9899a7e0f789992e682726a96fcee6fcc7855
Christian Heinemann <heinemann.christian@gmail.com>
2021-02-02 19:28:53 +0100

    + new examples + allowing editor mode from zoom level 4 + lighten yellow cell color

--- source/ModelBasic/Colors.h
+++ source/ModelBasic/Colors.h
@@ -5,7 +5,7 @@ namespace Const
     unsigned int const IndividualCellColor1 = 0xb05070ff;
     unsigned int const IndividualCellColor2 = 0xb0ff6040;
     unsigned int const IndividualCellColor3 = 0xb070ff50;
-    unsigned int const IndividualCellColor4 = 0xb0ffbf50;
+    unsigned int const IndividualCellColor4 = 0xb0ffdf50;
     unsigned int const IndividualCellColor5 = 0xb0bf50ff;
     unsigned int const IndividualCellColor6 = 0xb050ffef;
     unsigned int const IndividualCellColor7 = 0xb0bfbfbf;
