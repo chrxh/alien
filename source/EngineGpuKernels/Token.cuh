6b634aa39497a3f1f4e9426c631d6d7489b7091d
Christian Heinemann <heinemann.christian@gmail.com>
2021-04-07 09:58:27 +0200

    quick fix for preventing negative energies but a thorough analysis is need

--- source/EngineGpuKernels/Token.cuh
+++ source/EngineGpuKernels/Token.cuh
@@ -29,6 +29,13 @@ struct Token
         return _energy;
     }
 
+    __device__ __inline__ void repair()
+    {
+        if (isnan(_energy) || _energy < 0.0) {
+            _energy = 0.01;
+        }
+    }
+
 private:
     float _energy;
 };
