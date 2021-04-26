4b27204f6e091fd431db2033d44e9babf171d063
Christian Heinemann <heinemann.christian@gmail.com>
2021-04-26 18:47:58 +0200

    testing next example

--- source/Gui/VectorUniverseView.cpp
+++ source/Gui/VectorUniverseView.cpp
@@ -6,7 +6,7 @@
 #include <QScrollBar>
 #include <QResizeEvent>
 #include <QtGui>
-#include <QOpenGLFunctions>
+#include <QOpenGLExtraFunctions>
 #include <QMatrix4x4>
 #include <QOpenGLWidget>
 #include <QOpenGLShaderProgram>
@@ -28,11 +28,38 @@
 #include "VectorImageSectionItem.h"
 #include "VectorViewport.h"
 
+            float vertices[] = {
+    0.5f,
+    0.5f,
+    0.0f,  // top right
+    0.5f,
+    -0.5f,
+    0.0f,  // bottom right
+    -0.5f,
+    -0.5f,
+    0.0f,  // bottom left
+    -0.5f,
+    0.5f,
+    0.0f  // top left
+};
+unsigned int indices[] = {
+    // note that we start from 0!
+    0,
+    1,
+    3,  // first Triangle
+    1,
+    2,
+    3  // second Triangle
+};
+
+#include <iostream>
+
 class VectorViewGraphicsScene
     : public QGraphicsScene
-    , protected QOpenGLFunctions
+    , protected QOpenGLExtraFunctions
 {
 public:
+/*
     const char* vertexShaderSource = "attribute highp vec4 posAttr;\n"
                                             "attribute lowp vec4 colAttr;\n"
                                             "varying lowp vec4 col;\n"
@@ -46,6 +73,22 @@ public:
                                               "void main() {\n"
                                               "   gl_FragColor = col;\n"
                                               "}\n";
+*/
+    const char* vertexShaderSource = "#version 330 core\n"
+                                     "layout (location = 0) in vec3 aPos;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                     "}\0";
+    const char* fragmentShaderSource = "#version 330 core\n"
+                                       "out vec4 FragColor;\n"
+                                       "void main()\n"
+                                       "{\n"
+                                       "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                       "}\n\0";
+
+    unsigned int shaderProgram;
+    unsigned int VBO, VAO, EBO;
 
     VectorViewGraphicsScene(IntVector2D const& displaySize, std::mutex& mutex, QObject* parent = nullptr)
         : QGraphicsScene(parent)
@@ -55,6 +98,52 @@ public:
         m_context = new QOpenGLContext;
         m_context->create();
         initializeOpenGLFunctions();
+
+                        unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+        glCompileShader(vertexShader);
+        // check for shader compile errors
+        int success;
+        char infoLog[512];
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+        // fragment shader
+        unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+        glCompileShader(fragmentShader);
+        // check for shader compile errors
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+
+        // link shaders
+        shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+        // check for linking errors
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+        if (!success) {
+            glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        }
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
+        // set up vertex data (and buffer(s)) and configure vertex attributes
+        // ------------------------------------------------------------------
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+        // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+        glBindVertexArray(VAO);
+
+        /*
         m_program = new QOpenGLShaderProgram(this);
         m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
         m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
@@ -66,7 +155,14 @@ public:
         m_matrixUniform = m_program->uniformLocation("matrix");
         Q_ASSERT(m_matrixUniform != -1);
 
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+        float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+*/
         _image = boost::make_shared<QImage>(_displaySize.x, _displaySize.y, QImage::Format_ARGB32);
+
     }
 
     void resize(IntVector2D const& size) { _image = boost::make_shared<QImage>(size.x, size.y, QImage::Format_ARGB32); }
@@ -80,6 +176,35 @@ public:
     {
         std::lock_guard<std::mutex> lock(_mutex);
 
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+        //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+        // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+        // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+        glBindVertexArray(0);
+//--------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(
+            VAO);  // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        /*
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -93,7 +218,6 @@ public:
         m_program->setUniformValue(m_matrixUniform, matrix);
 
         static const GLfloat vertices[] = {-1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f};
-
         static const GLfloat colors[] = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f};
 
         glVertexAttribPointer(m_posAttr, 2, GL_FLOAT, GL_FALSE, 0, vertices);
@@ -108,6 +232,7 @@ public:
         glDisableVertexAttribArray(m_posAttr);
 
         m_program->release();
+*/
 
         //        painter->drawImage(0, 0, *_image);
     }
