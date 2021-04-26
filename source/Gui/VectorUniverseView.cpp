bdb479836b5d7ac9f57d3dc87803d8fc623653bc
Christian Heinemann <heinemann.christian@gmail.com>
2021-04-26 22:55:42 +0200

    render texture of simulation view in OpenGL

--- source/Gui/VectorUniverseView.cpp
+++ source/Gui/VectorUniverseView.cpp
@@ -28,19 +28,12 @@
 #include "VectorImageSectionItem.h"
 #include "VectorViewport.h"
 
-            float vertices[] = {
-    0.5f,
-    0.5f,
-    0.0f,  // top right
-    0.5f,
-    -0.5f,
-    0.0f,  // bottom right
-    -0.5f,
-    -0.5f,
-    0.0f,  // bottom left
-    -0.5f,
-    0.5f,
-    0.0f  // top left
+float vertices[] = {
+    // positions          // colors           // texture coords
+    1.0f,  1.0f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,  // top right
+    1.0f,  -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,  // bottom right
+    -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,  // bottom left
+    -1.0f, 1.0f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f  // top left
 };
 unsigned int indices[] = {
     // note that we start from 0!
@@ -75,17 +68,27 @@ public:
                                               "}\n";
 */
     const char* vertexShaderSource = "#version 330 core\n"
-                                     "layout (location = 0) in vec3 aPos;\n"
+                                     "layout(location = 0) in vec3 aPos;\n"
+                                     "layout(location = 1) in vec3 aColor;\n"
+                                     "layout(location = 2) in vec2 aTexCoord;\n"
+                                     "out vec3 ourColor;\n"
+                                     "out vec2 TexCoord;\n"
                                      "void main()\n"
-                                     "{\n"
-                                     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                     "\n{"
+                                     "  gl_Position = vec4(aPos, 1.0);\n"
+                                     "  ourColor = aColor;\n"
+                                     "  TexCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
                                      "}\0";
-    const char* fragmentShaderSource = "#version 330 core\n"
-                                       "out vec4 FragColor;\n"
-                                       "void main()\n"
-                                       "{\n"
-                                       "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                       "}\n\0";
+    const char* fragmentShaderSource =
+        "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "in vec3 ourColor;\n"
+        "in vec2 TexCoord;\n"
+        "uniform sampler2D texture1;\n"
+        "void main()\n"
+        "{\n"
+        "    FragColor = texture(texture1, TexCoord);\n"
+        "}\0";
 
     unsigned int shaderProgram;
     unsigned int VBO, VAO, EBO;
@@ -99,7 +102,7 @@ public:
         m_context->create();
         initializeOpenGLFunctions();
 
-                        unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
         glCompileShader(vertexShader);
         // check for shader compile errors
@@ -126,6 +129,7 @@ public:
         glAttachShader(shaderProgram, vertexShader);
         glAttachShader(shaderProgram, fragmentShader);
         glLinkProgram(shaderProgram);
+
         // check for linking errors
         glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
         if (!success) {
@@ -143,24 +147,6 @@ public:
         // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
         glBindVertexArray(VAO);
 
-        /*
-        m_program = new QOpenGLShaderProgram(this);
-        m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
-        m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
-        m_program->link();
-        m_posAttr = m_program->attributeLocation("posAttr");
-        Q_ASSERT(m_posAttr != -1);
-        m_colAttr = m_program->attributeLocation("colAttr");
-        Q_ASSERT(m_colAttr != -1);
-        m_matrixUniform = m_program->uniformLocation("matrix");
-        Q_ASSERT(m_matrixUniform != -1);
-
-        glGenBuffers(1, &VBO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-        float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-*/
         _image = boost::make_shared<QImage>(_displaySize.x, _displaySize.y, QImage::Format_ARGB32);
 
     }
@@ -175,14 +161,47 @@ public:
     void drawBackground(QPainter* painter, const QRectF& rect) override
     {
         std::lock_guard<std::mutex> lock(_mutex);
-
+        
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+        // position attribute
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
+        // color attribute
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+        // texture coord attribute
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+
+
+        
+        unsigned int texture1;
+        // texture 1
+        // ---------
+        glGenTextures(1, &texture1);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        // set the texture wrapping parameters
+        glTexParameteri(
+            GL_TEXTURE_2D,
+            GL_TEXTURE_WRAP_S,
+            GL_REPEAT);  // set texture wrapping to GL_REPEAT (default wrapping method)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        // set texture filtering parameters
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        // load image, create texture and generate mipmaps
+        int width, height, nrChannels;
+        glTexImage2D(
+            GL_TEXTURE_2D, 0, GL_RGBA, _image->width(), _image->height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, _image->bits());
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+       glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0);
+
 
         // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
         glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -194,16 +213,23 @@ public:
         // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
         glBindVertexArray(0);
 //--------
+/*
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+*/
+
+        // bind textures on corresponding texture units
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
 
         // draw our first triangle
         glUseProgram(shaderProgram);
-        glBindVertexArray(
-            VAO);  // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+
+        glBindVertexArray(VAO); 
         //glDrawArrays(GL_TRIANGLES, 0, 6);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
+        glDeleteTextures(1, &texture1);
         /*
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
