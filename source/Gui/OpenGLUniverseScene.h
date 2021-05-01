be92c84a246c09dea58f70291e41bd46582cc6d7
Christian Heinemann <heinemann.christian@gmail.com>
2021-05-01 09:22:48 +0200

    using Qt wrapper class for OpenGL functions

--- source/Gui/OpenGLUniverseScene.h
+++ source/Gui/OpenGLUniverseScene.h
@@ -2,6 +2,9 @@
 
 #include <QGraphicsScene>
 #include <QOpenGLExtraFunctions>
+#include <QOpenGLBuffer>
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLTexture>
 
 #include "Definitions.h"
 
@@ -12,32 +15,6 @@ class OpenGLUniverseScene
     , protected QOpenGLExtraFunctions
 {
 public:
-    const char* vertexShaderSource = "#version 330 core\n"
-                                     "layout(location = 0) in vec3 aPos;\n"
-                                     "layout(location = 1) in vec3 aColor;\n"
-                                     "layout(location = 2) in vec2 aTexCoord;\n"
-                                     "out vec3 ourColor;\n"
-                                     "out vec2 TexCoord;\n"
-                                     "void main()\n"
-                                     "\n{"
-                                     "  gl_Position = vec4(aPos, 1.0);\n"
-                                     "  ourColor = aColor;\n"
-                                     "  TexCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
-                                     "}\0";
-    const char* fragmentShaderSource = "#version 330 core\n"
-                                       "out vec4 FragColor;\n"
-                                       "in vec3 ourColor;\n"
-                                       "in vec2 TexCoord;\n"
-                                       "uniform sampler2D texture1;\n"
-                                       "void main()\n"
-                                       "{\n"
-                                       "    FragColor = texture(texture1, TexCoord);\n"
-                                       "}\0";
-
-    unsigned int shaderProgram;
-    unsigned int VBO, VAO, EBO;
-    unsigned int texture;
-
     OpenGLUniverseScene(
         SimulationAccess* access,
         IntVector2D const& viewSize,
@@ -51,14 +28,17 @@ public:
     void drawBackground(QPainter* painter, const QRectF& rect) override;
 
 private:
+
     SimulationAccess* _access;
     std::mutex& _mutex;
     ImageResource _imageResource;
 
-    QOpenGLContext* m_context;
     GLint m_posAttr = 0;
     GLint m_colAttr = 0;
     GLint m_matrixUniform = 0;
 
+    QOpenGLBuffer m_vertex;
+    QOpenGLVertexArrayObject m_object;
     QOpenGLShaderProgram* m_program = nullptr;
+    QOpenGLTexture* m_texture = nullptr;
 };
---
+++ source/Gui/Resources/FragmentShader.txt
@@ -0,0 +1,8 @@
+#version 330 core
+out vec4 FragColor;
+in vec2 TexCoord;
+uniform sampler2D texture1;
+void main()
+{
+    FragColor = texture(texture1, TexCoord);
+}
