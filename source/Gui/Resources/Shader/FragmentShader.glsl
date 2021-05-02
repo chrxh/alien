1f2131f47834dbde4584d776e75627925d471ecc
Christian Heinemann <heinemann.christian@gmail.com>
2021-05-02 16:50:20 +0200

    making glow effect with fragment shader

--- source/Gui/Resources/Shader/FragmentShader.glsl
+++ source/Gui/Resources/Shader/FragmentShader.glsl
@@ -2,12 +2,28 @@
 out vec4 FragColor;
 in vec2 texCoord;
 uniform sampler2D texture1;
-uniform bool mirror;
+uniform int phase;
+
+uniform float weight[10] = float[](0.9, 0.12, 0.03, 0.03, 0.03, 0.03, 0.01, 0.01, 0.01, 0.01);
+
 void main()
 {
-    if (mirror) {
-        FragColor = texture(texture1, vec2(texCoord.x, 1.0 - texCoord.y));
+    vec2 texelSize = 1.0 / textureSize(texture1, 0);
+    if (phase == 0) {
+        vec2 mirroredCoord = vec2(texCoord.x, 1.0 - texCoord.y);  //mirror
+
+        vec3 result = texture(texture1, mirroredCoord).rgb * weight[0];
+        for (int i = 1; i < 10; ++i) {
+            result += texture(texture1, mirroredCoord + vec2(texelSize.x * i, 0.0)).rgb * weight[i];
+            result += texture(texture1, mirroredCoord - vec2(texelSize.x * i, 0.0)).rgb * weight[i];
+        }
+        FragColor = vec4(result, 1.0);
     } else {
-        FragColor = texture(texture1, texCoord);
+        vec3 result = texture(texture1, texCoord).rgb * weight[0];
+        for (int i = 1; i < 10; ++i) {
+            result += texture(texture1, texCoord + vec2(0.0, texelSize.y * i)).rgb * weight[i];
+            result += texture(texture1, texCoord - vec2(0.0, texelSize.y * i)).rgb * weight[i];
+        }
+        FragColor = vec4(result, 1.0);
     }
 }
