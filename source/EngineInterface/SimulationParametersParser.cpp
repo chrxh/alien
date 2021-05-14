04f0260495205bdc5ec28a111eb6fa4f40ce605a
Christian Heinemann <heinemann.christian@gmail.com>
2021-05-14 17:26:16 +0200

    converter 2

--- source/EngineInterface/SimulationParametersParser.cpp
+++ source/EngineInterface/SimulationParametersParser.cpp
@@ -27,7 +27,7 @@ boost::property_tree::ptree SimulationParametersParser::encode(SimulationParamet
     tree.add("cell.function.weapon.strength", toString(parameters.cellFunctionWeaponStrength));
     tree.add("cell.function.weapon.energy cost", toString(parameters.cellFunctionWeaponEnergyCost));
     tree.add(
-        "cell.function.constructor.offspring.cellEnergy",
+        "cell.function.constructor.offspring.cell energy",
         toString(parameters.cellFunctionConstructorOffspringCellEnergy));
     tree.add(
         "cell.function.constructor.offspring.cell distance",
@@ -66,52 +66,52 @@ boost::property_tree::ptree SimulationParametersParser::encode(SimulationParamet
 SimulationParameters SimulationParametersParser::decode(boost::property_tree::ptree const& tree)
 {
     SimulationParameters result;
-    result.cellMinDistance = tree.get<float>("cell.minDistance");
-    result.cellMaxDistance = tree.get<float>("cell.maxDistance");
-    result.cellMaxForce = tree.get<float>("cell.maxForce");
-    result.cellMaxForceDecayProb = tree.get<float>("cell.maxForceDecayProbability");
-    result.cellMinTokenUsages = tree.get<int>("cell.minTokenUsages");
-    result.cellTokenUsageDecayProb = tree.get<float>("cell.tokenUsageDecayProbability");
-    result.cellMaxBonds = tree.get<int>("cell.maxBonds");
-    result.cellMaxToken = tree.get<int>("cell.maxToken");
-    result.cellMaxTokenBranchNumber = tree.get<int>("cell.maxTokenBranchNumber");
-    result.cellMinEnergy = tree.get<float>("cell.minEnergy");
-    result.cellTransformationProb = tree.get<float>("cell.transformationProb");
-    result.cellFusionVelocity = tree.get<float>("cell.fusionVelocity");
-    result.cellFunctionComputerMaxInstructions = tree.get<int>("cell.function.computer.maxInstructions");
-    result.cellFunctionComputerCellMemorySize = tree.get<int>("cell.function.computer.memorySize");
+    result.cellMinDistance = tree.get<float>("cell.min distance");
+    result.cellMaxDistance = tree.get<float>("cell.max distance");
+    result.cellMaxForce = tree.get<float>("cell.max force");
+    result.cellMaxForceDecayProb = tree.get<float>("cell.max force decay probability");
+    result.cellMinTokenUsages = tree.get<int>("cell.min token usages");
+    result.cellTokenUsageDecayProb = tree.get<float>("cell.token usage decay probability");
+    result.cellMaxBonds = tree.get<int>("cell.max bonds");
+    result.cellMaxToken = tree.get<int>("cell.max token");
+    result.cellMaxTokenBranchNumber = tree.get<int>("cell.max token branch number");
+    result.cellMinEnergy = tree.get<float>("cell.min energy");
+    result.cellTransformationProb = tree.get<float>("cell.transformation probability");
+    result.cellFusionVelocity = tree.get<float>("cell.fusion velocity");
+    result.cellFunctionComputerMaxInstructions = tree.get<int>("cell.function.computer.max instructions");
+    result.cellFunctionComputerCellMemorySize = tree.get<int>("cell.function.computer.memory size");
     result.cellFunctionWeaponStrength = tree.get<float>("cell.function.weapon.strength");
-    result.cellFunctionWeaponEnergyCost = tree.get<float>("cell.function.weapon.energyCost");
+    result.cellFunctionWeaponEnergyCost = tree.get<float>("cell.function.weapon.energy cost");
     result.cellFunctionConstructorOffspringCellEnergy =
-        tree.get<float>("cell.function.constructor.offspringCellEnergy");
+        tree.get<float>("cell.function.constructor.offspring.cellEnergy");
     result.cellFunctionConstructorOffspringCellDistance =
         tree.get<float>(
-        "cell.function.constructor.offspringCellDistance");
+        "cell.function.constructor.offspring.cell distance");
     result.cellFunctionConstructorOffspringTokenEnergy =
-        tree.get<float>("cell.function.constructor.offspringTokenEnergy");
+        tree.get<float>("cell.function.constructor.offspring.token energy");
     result.cellFunctionConstructorOffspringTokenSuppressMemoryCopy=
-        tree.get<bool>("cell.function.constructor.offspringTokenSuppressMemoryCopy", false);
+        tree.get<bool>("cell.function.constructor.offspring.token suppress memory copy", false);
     result.cellFunctionConstructorTokenDataMutationProb =
         tree.get<float>(
-        "cell.function.constructor.tokenDataMutationProbability");
+        "cell.function.constructor.mutation probability.token data");
     result.cellFunctionConstructorCellDataMutationProb =
         tree.get<float>(
-        "cell.function.constructor.cellDataMutationProbability");
+        "cell.function.constructor.mutation probability.cell data");
     result.cellFunctionConstructorCellPropertyMutationProb =
         tree.get<float>(
-        "cell.function.constructor.cellPropertyMutationProbability");
+        "cell.function.constructor.mutation probability.cell property");
     result.cellFunctionConstructorCellStructureMutationProb =
         tree.get<float>(
-        "cell.function.constructor.cellStructureMutationProbability");
+        "cell.function.constructor.mutation probability.cell structure");
     result.cellFunctionSensorRange = tree.get<float>("cell.function.sensor.range");
     result.cellFunctionCommunicatorRange = tree.get<float>("cell.function.communicator.range");
-    result.tokenMemorySize = tree.get<int>("token.memorySize");
-    result.tokenMinEnergy = tree.get<float>("token.minEnergy");
+    result.tokenMemorySize = tree.get<int>("token.memory size");
+    result.tokenMinEnergy = tree.get<float>("token.min energy");
     result.radiationExponent = tree.get<float>("radiation.exponent");
     result.radiationFactor = tree.get<float>("radiation.factor");
     result.radiationProb = tree.get<float>("radiation.probability");
-    result.radiationVelocityMultiplier = tree.get<float>("radiation.velocityMultiplier");
-    result.radiationVelocityPerturbation = tree.get<float>("radiation.velocityPerturbation");
+    result.radiationVelocityMultiplier = tree.get<float>("radiation.velocity multiplier");
+    result.radiationVelocityPerturbation = tree.get<float>("radiation.velocity perturbation");
 
     return result;
 }
