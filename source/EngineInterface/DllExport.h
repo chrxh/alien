0d08ba166c8a587b873f6ac5fe97d3f63da37622
Mauro Persano <mpersano.dev@gmail.com>
2021-06-12 09:21:21 -0300

    Only use the __declspec decorator on Windows

--- source/EngineInterface/DllExport.h
+++ source/EngineInterface/DllExport.h
@@ -2,7 +2,7 @@
 
 #include <QtCore/qglobal.h>
 
-#ifndef ALIEN_STATIC
+#if defined(_WIN32) && !defined(ALIEN_STATIC)
 #ifdef ENGINEINTERFACE_LIB
 # define ENGINEINTERFACE_EXPORT Q_DECL_EXPORT
 #else
