12b596a292e55a55e12b87b568c51dcc7a8bfbcc
Mauro Persano <mpersano.dev@gmail.com>
2021-06-14 18:12:53 -0300

    Fix GlobalFactory not getting registered

--- source/Base/BaseServices.cpp
+++ source/Base/BaseServices.cpp
@@ -2,11 +2,14 @@
 
 #include "ServiceLocator.h"
 #include "LoggingServiceImpl.h"
+#include "GlobalFactoryImpl.h"
 #include "BaseServices.h"
 
 BaseServices::BaseServices()
 {
     static LoggingServiceImpl loggingServiceImpl;
+    static GlobalFactoryImpl globalFactoryImpl;
 
     ServiceLocator::getInstance().registerService<LoggingService>(&loggingServiceImpl);
+	ServiceLocator::getInstance().registerService<GlobalFactory>(&globalFactoryImpl);
 }
