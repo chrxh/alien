c03a402a2bac2142831df10cd14036ec7f4d49e3
Christian Heinemann <heinemann.christian@gmail.com>
2021-10-13 21:54:48 +0200

    + choosing color in colorize dialog + bug in loading flow flied centers fixed

--- source/Gui/ColorizeDialog.h
+++ source/Gui/ColorizeDialog.h
@@ -13,9 +13,18 @@ public:
     void show();
 
 private:
+    void checkbox(std::string id, uint64_t cellColor, bool& check);
+
     void onColorize();
 
     SimulationController _simController;
 
     bool _show = false;
+    bool _checkColor1 = false;
+    bool _checkColor2 = false;
+    bool _checkColor3 = false;
+    bool _checkColor4 = false;
+    bool _checkColor5 = false;
+    bool _checkColor6 = false;
+    bool _checkColor7 = false;
 };
