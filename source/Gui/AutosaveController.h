c768c0f2d544cb411dd73a89d72e82474e6e0e28
Christian Heinemann <heinemann.christian@gmail.com>
2021-10-29 16:49:59 +0200

    adding autosave

---
+++ source/Gui/AutosaveController.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <chrono>
+
+#include "EngineImpl/SimulationController.h"
+#include "Definitions.h"
+
+class _AutosaveController
+{
+public:
+    _AutosaveController(SimulationController const& simController);
+    ~_AutosaveController();
+
+    void shutdown();
+
+    bool isOn() const;
+    void setOn(bool value);
+
+    void process();
+
+private:
+    void onSave();
+
+    SimulationController _simController;
+
+    bool _on = true;
+    boost::optional<std::chrono::steady_clock::time_point> _startTimePoint;
+};
