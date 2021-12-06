51f2b30d6efa46c3f07a9fd760df7b26adf89270
Christian Heinemann <heinemann.christian@gmail.com>
2021-12-06 20:54:53 +0100

    converting to refactored description

--- source/EngineInterface/Descriptions.cpp
+++ source/EngineInterface/Descriptions.cpp
@@ -83,6 +83,55 @@ bool CellDescription::isConnectedTo(uint64_t id) const
         != connections->end();
 }
 
+CellDescription2::CellDescription2(CellChangeDescription const& change)
+{
+    id = change.id;
+    pos = *static_cast<boost::optional<RealVector2D>>(change.pos);
+    energy = *static_cast<boost::optional<double>>(change.energy);
+    maxConnections = *static_cast<boost::optional<int>>(change.maxConnections);
+
+    std::list<ConnectionDescription> connectionList =
+        *convert(static_cast<boost::optional<std::list<ConnectionChangeDescription>>>(change.connectingCells));
+    connections = std::vector<ConnectionDescription>(connectionList.begin(), connectionList.end());
+    tokenBlocked = *change.tokenBlocked.getOptionalValue();  //static_cast<boost::optional<bool>> doesn't work for some reason
+    tokenBranchNumber = *static_cast<boost::optional<int>>(change.tokenBranchNumber);
+    metadata = *static_cast<boost::optional<CellMetadata>>(change.metadata);
+    cellFeature =
+        *static_cast<boost::optional<CellFeatureDescription>>(change.cellFeatures);
+    for (auto const& token : *static_cast<boost::optional<vector<TokenDescription>>>(change.tokens)) {
+        auto newToken = Converter::convert2(token);
+        tokens.emplace_back(newToken);
+    }
+    tokenUsages = *static_cast<boost::optional<int>>(change.tokenUsages);
+}
+
+CellDescription2& CellDescription2::addToken(TokenDescription2 const& value)
+{
+    tokens.emplace_back(value);
+    return *this;
+}
+
+CellDescription2& CellDescription2::addToken(int index, TokenDescription2 const& value)
+{
+    tokens.insert(tokens.begin() + index, value);
+    return *this;
+}
+
+CellDescription2& CellDescription2::delToken(int index)
+{
+    tokens.erase(tokens.begin() + index);
+    return *this;
+}
+
+bool CellDescription2::isConnectedTo(uint64_t id) const
+{
+    return std::find_if(
+               connections.begin(),
+               connections.end(),
+               [&id](auto const& connection) { return connection.cellId == id; })
+        != connections.end();
+}
+
 ClusterDescription& ClusterDescription::addConnection(
     uint64_t const& cellId1,
     uint64_t const& cellId2,
@@ -211,6 +260,121 @@ CellDescription& ClusterDescription::getCellRef(uint64_t const& cellId, std::uno
     THROW_NOT_IMPLEMENTED();
 }
 
+ClusterDescription2& ClusterDescription2::addConnection(
+    uint64_t const& cellId1,
+    uint64_t const& cellId2,
+    std::unordered_map<uint64_t, int>& cache)
+{
+    auto& cell1 = getCellRef(cellId1, cache);
+    auto& cell2 = getCellRef(cellId2, cache);
+
+    auto addConnection = [this, &cache](auto& cell, auto& otherCell) {
+        CHECK(cell.connections.size() < cell.maxConnections);
+
+        auto newAngle = Math::angleOfVector(otherCell.pos - cell.pos);
+
+        if (cell.connections.empty()) {
+            ConnectionDescription newConnection;
+            newConnection.cellId = otherCell.id;
+            newConnection.distance = toFloat(Math::length(otherCell.pos - cell.pos));
+            newConnection.angleFromPrevious = 360.0;
+            cell.connections.emplace_back(newConnection);
+            return;
+        }
+        if (1 == cell.connections.size()) {
+            ConnectionDescription newConnection;
+            newConnection.cellId = otherCell.id;
+            newConnection.distance = toFloat(Math::length(otherCell.pos - cell.pos));
+
+            auto connectedCell = getCellRef(cell.connections.front().cellId, cache);
+            auto connectedCellDelta = connectedCell.pos - cell.pos;
+            auto prevAngle = Math::angleOfVector(connectedCellDelta);
+            auto angleDiff = newAngle - prevAngle;
+            if (angleDiff >= 0) {
+                newConnection.angleFromPrevious = toFloat(angleDiff);
+                cell.connections.begin()->angleFromPrevious = 360.0f - toFloat(angleDiff);
+            } else {
+                newConnection.angleFromPrevious = 360.0f + toFloat(angleDiff);
+                cell.connections.begin()->angleFromPrevious = toFloat(-angleDiff);
+            }
+            cell.connections.emplace_back(newConnection);
+            return;
+        }
+
+        auto firstConnectedCell = getCellRef(cell.connections.front().cellId, cache);
+        auto firstConnectedCellDelta = firstConnectedCell.pos - cell.pos;
+        auto angle = Math::angleOfVector(firstConnectedCellDelta);
+        auto connectionIt = ++cell.connections.begin();
+        while (true) {
+            auto nextAngle = angle + connectionIt->angleFromPrevious;
+
+            if ((angle < newAngle && newAngle <= nextAngle)
+                || (angle < (newAngle + 360.0f) && (newAngle + 360.0f) <= nextAngle)) {
+                break;
+            }
+
+            ++connectionIt;
+            if (connectionIt == cell.connections.end()) {
+                connectionIt = cell.connections.begin();
+            }
+            angle = nextAngle;
+            if (angle > 360.0f) {
+                angle -= 360.0f;
+            }
+        }
+
+        ConnectionDescription newConnection;
+        newConnection.cellId = otherCell.id;
+        newConnection.distance = toFloat(Math::length(otherCell.pos - cell.pos));
+
+        auto angleDiff1 = newAngle - angle;
+        if (angleDiff1 < 0) {
+            angleDiff1 += 360.0f;
+        }
+        auto angleDiff2 = connectionIt->angleFromPrevious;
+
+        auto factor = (angleDiff2 != 0) ? angleDiff1 / angleDiff2 : 0.5f;
+        newConnection.angleFromPrevious = toFloat(angleDiff2 * factor);
+        connectionIt = cell.connections.insert(connectionIt, newConnection);
+        ++connectionIt;
+        if (connectionIt == cell.connections.end()) {
+            connectionIt = cell.connections.begin();
+        }
+        connectionIt->angleFromPrevious = toFloat(angleDiff2 * (1 - factor));
+    };
+
+    addConnection(cell1, cell2);
+    addConnection(cell2, cell1);
+
+    return *this;
+}
+
+RealVector2D ClusterDescription2::getClusterPosFromCells() const
+{
+    RealVector2D result;
+    for (auto const& cell : cells) {
+        result += cell.pos;
+    }
+    result /= cells.size();
+    return result;
+}
+
+CellDescription2& ClusterDescription2::getCellRef(uint64_t const& cellId, std::unordered_map<uint64_t, int>& cache)
+{
+    auto findResult = cache.find(cellId);
+    if (findResult != cache.end()) {
+        return cells.at(findResult->second);
+    }
+    for (int i = 0; i < cells.size(); ++i) {
+        auto& cell = cells.at(i);
+        if (cell.id == cellId) {
+            cache.emplace(cellId, i);
+            return cell;
+        }
+    }
+    THROW_NOT_IMPLEMENTED();
+}
+
 ParticleDescription::ParticleDescription(ParticleChangeDescription const & change)
 {
 	id = change.id;
@@ -218,6 +382,15 @@ ParticleDescription::ParticleDescription(ParticleChangeDescription const & chang
 	vel = static_cast<boost::optional<RealVector2D>>(change.vel);
 	energy = static_cast<boost::optional<double>>(change.energy);
 	metadata = static_cast<boost::optional<ParticleMetadata>>(change.metadata);
+}
+
+ParticleDescription2::ParticleDescription2(ParticleChangeDescription const& change)
+{
+    id = change.id;
+    pos = *static_cast<boost::optional<RealVector2D>>(change.pos);
+    vel = *static_cast<boost::optional<RealVector2D>>(change.vel);
+    energy = *static_cast<boost::optional<double>>(change.energy);
+    metadata = *static_cast<boost::optional<ParticleMetadata>>(change.metadata);
 }
 
 RealVector2D DataDescription::calcCenter() const
@@ -260,4 +433,96 @@ void DataDescription::shift(RealVector2D const & delta)
 			*particle.pos += delta;
 		}
 	}
+}
+
+RealVector2D DataDescription2::calcCenter() const
+{
+    RealVector2D result;
+    int numEntities = 0;
+    for (auto const& cluster : clusters) {
+        for (auto const& cell : cluster.cells) {
+            result += cell.pos;
+            ++numEntities;
+        }
+    }
+    for (auto const& particle : particles) {
+        result += particle.pos;
+        ++numEntities;
+    }
+    result /= numEntities;
+    return result;
+}
+
+void DataDescription2::shift(RealVector2D const& delta)
+{
+    for (auto& cluster : clusters) {
+        for (auto& cell : cluster.cells) {
+            cell.pos += delta;
+        }
+    }
+    for (auto& particle : particles) {
+        particle.pos += delta;
+    }
+}
+
+TokenDescription2 Converter::convert2(TokenDescription const& token)
+{
+    TokenDescription2 result;
+    result.data = *token.data;
+    result.energy = *token.energy;
+    return result;
+}
+
+CellDescription2 Converter::convert2(CellDescription const& cell)
+{
+    CellDescription2 result;
+    result.id = cell.id;
+
+    result.pos = *cell.pos;
+    result.vel = *cell.vel;
+    result.energy = *cell.energy;
+    result.maxConnections = *cell.maxConnections;
+    result.connections = std::vector<ConnectionDescription>(cell.connections->begin(), cell.connections->end());
+    result.tokenBlocked = *cell.tokenBlocked;
+    result.tokenBranchNumber = *cell.tokenBranchNumber;
+    result.metadata = *cell.metadata;
+    result.cellFeature = *cell.cellFeature;
+    for (auto const& token : *cell.tokens) {
+        result.tokens.emplace_back(convert2(token));
+    }
+    result.tokenUsages = *cell.tokenUsages;
+    return result;
+}
+
+ClusterDescription2 Converter::convert2(ClusterDescription const& cluster)
+{
+    ClusterDescription2 result;
+    result.id = cluster.id;
+    for (auto const& cell : *cluster.cells) {
+        result.cells.emplace_back(convert2(cell));
+    }
+    return result;
+}
+
+ParticleDescription2 Converter::convert2(ParticleDescription const& particle)
+{
+    ParticleDescription2 result;
+    result.id = particle.id;
+    result.pos = *particle.pos;
+    result.vel = *particle.vel;
+    result.energy = *particle.energy;
+    result.metadata = *particle.metadata;
+    return result;
+}
+
+DataDescription2 Converter::convert2(DataDescription const& data)
+{
+    DataDescription2 result;
+    for (auto const& cluster : *data.clusters) {
+        result.clusters.emplace_back(convert2(cluster));
+    }
+    for (auto const& particle : *data.particles) {
+        result.particles.emplace_back(convert2(particle));
+    }
+    return result;
 }
--- source/EngineInterface/Descriptions.h
+++ source/EngineInterface/Descriptions.h
@@ -57,6 +57,25 @@ struct TokenDescription
 	bool operator!=(TokenDescription const& other) const { return !operator==(other); }
 };
 
+struct TokenDescription2
+{
+    double energy = 0;
+    std::string data;
+
+    TokenDescription2& setEnergy(double value)
+    {
+        energy = value;
+        return *this;
+    }
+    TokenDescription2& setData(std::string const& value)
+    {
+        data = value;
+        return *this;
+    }
+    bool operator==(TokenDescription2 const& other) const { return energy == other.energy && data == other.data; }
+    bool operator!=(TokenDescription2 const& other) const { return !operator==(other); }
+};
+
 struct ConnectionDescription
 {
     uint64_t cellId;
@@ -149,6 +168,90 @@ struct CellDescription
     ENGINEINTERFACE_EXPORT bool isConnectedTo(uint64_t id) const;
 };
 
+struct CellDescription2
+{
+    uint64_t id = 0;
+
+    RealVector2D pos;
+    RealVector2D vel;
+    double energy;
+    int maxConnections;
+    std::vector<ConnectionDescription> connections;
+    bool tokenBlocked;
+    int tokenBranchNumber;
+    CellMetadata metadata;
+    CellFeatureDescription cellFeature;
+    std::vector<TokenDescription2> tokens;
+    int tokenUsages;
+
+    ENGINEINTERFACE_EXPORT CellDescription2() = default;
+    ENGINEINTERFACE_EXPORT CellDescription2(CellChangeDescription const& change);
+    CellDescription2& setId(uint64_t value)
+    {
+        id = value;
+        return *this;
+    }
+    CellDescription2& setPos(RealVector2D const& value)
+    {
+        pos = value;
+        return *this;
+    }
+    CellDescription2& setVel(RealVector2D const& value)
+    {
+        vel = value;
+        return *this;
+    }
+    CellDescription2& setEnergy(double value)
+    {
+        energy = value;
+        return *this;
+    }
+    CellDescription2& setMaxConnections(int value)
+    {
+        maxConnections = value;
+        return *this;
+    }
+    CellDescription2& setConnectingCells(std::vector<ConnectionDescription> const& value)
+    {
+        connections = value;
+        return *this;
+    }
+    CellDescription2& setFlagTokenBlocked(bool value)
+    {
+        tokenBlocked = value;
+        return *this;
+    }
+    CellDescription2& setTokenBranchNumber(int value)
+    {
+        tokenBranchNumber = value;
+        return *this;
+    }
+    CellDescription2& setMetadata(CellMetadata const& value)
+    {
+        metadata = value;
+        return *this;
+    }
+    CellDescription2& setCellFeature(CellFeatureDescription const& value)
+    {
+        cellFeature = value;
+        return *this;
+    }
+    CellDescription2& setTokens(vector<TokenDescription2> const& value)
+    {
+        tokens = value;
+        return *this;
+    }
+    ENGINEINTERFACE_EXPORT CellDescription2& addToken(TokenDescription2 const& value);
+    ENGINEINTERFACE_EXPORT CellDescription2& addToken(int index, TokenDescription2 const& value);
+    ENGINEINTERFACE_EXPORT CellDescription2& delToken(int index);
+    CellDescription2& setTokenUsages(int value)
+    {
+        tokenUsages = value;
+        return *this;
+    }
+    ENGINEINTERFACE_EXPORT bool isConnectedTo(uint64_t id) const;
+};
+
 struct ClusterDescription
 {
 	uint64_t id = 0;
@@ -187,6 +290,39 @@ private:
     CellDescription& getCellRef(uint64_t const& cellId, std::unordered_map<uint64_t, int>& cache);
 };
 
+struct ClusterDescription2
+{
+    uint64_t id = 0;
+
+    std::vector<CellDescription2> cells;
+
+    ENGINEINTERFACE_EXPORT ClusterDescription2() = default;
+
+    ClusterDescription2& setId(uint64_t value)
+    {
+        id = value;
+        return *this;
+    }
+    ClusterDescription2& addCells(list<CellDescription2> const& value)
+    {
+        cells.insert(cells.end(), value.begin(), value.end());
+        return *this;
+    }
+    ClusterDescription2& addCell(CellDescription2 const& value)
+    {
+        addCells({value});
+        return *this;
+    }
+
+    ENGINEINTERFACE_EXPORT ClusterDescription2&
+    addConnection(uint64_t const& cellId1, uint64_t const& cellId2, std::unordered_map<uint64_t, int>& cache);
+
+    ENGINEINTERFACE_EXPORT RealVector2D getClusterPosFromCells() const;
+
+private:
+    CellDescription2& getCellRef(uint64_t const& cellId, std::unordered_map<uint64_t, int>& cache);
+};
+
 struct ParticleDescription
 {
 	uint64_t id = 0;
@@ -219,6 +355,44 @@ struct ParticleDescription
         return *this;
     }
     ParticleDescription& setMetadata(ParticleMetadata const& value)
+    {
+        metadata = value;
+        return *this;
+    }
+};
+
+struct ParticleDescription2
+{
+    uint64_t id = 0;
+
+    RealVector2D pos;
+    RealVector2D vel;
+    double energy;
+    ParticleMetadata metadata;
+
+    ENGINEINTERFACE_EXPORT ParticleDescription2() = default;
+    ENGINEINTERFACE_EXPORT ParticleDescription2(ParticleChangeDescription const& change);
+    ParticleDescription2& setId(uint64_t value)
+    {
+        id = value;
+        return *this;
+    }
+    ParticleDescription2& setPos(RealVector2D const& value)
+    {
+        pos = value;
+        return *this;
+    }
+    ParticleDescription2& setVel(RealVector2D const& value)
+    {
+        vel = value;
+        return *this;
+    }
+    ParticleDescription2& setEnergy(double value)
+    {
+        energy = value;
+        return *this;
+    }
+    ParticleDescription2& setMetadata(ParticleMetadata const& value)
     {
         metadata = value;
         return *this;
@@ -280,23 +454,64 @@ struct DataDescription
 	void shift(RealVector2D const& delta);
 };
 
-struct ResolveDescription
+struct DataDescription2
 {
-	bool resolveIds = true;
-	bool resolveCellLinks = true;
+    vector<ClusterDescription2> clusters;
+    vector<ParticleDescription2> particles;
+
+    ENGINEINTERFACE_EXPORT DataDescription2() = default;
+    DataDescription2& addClusters(list<ClusterDescription2> const& value)
+    {
+        clusters.insert(clusters.end(), value.begin(), value.end());
+        return *this;
+    }
+    DataDescription2& addCluster(ClusterDescription2 const& value)
+    {
+        addClusters({value});
+        return *this;
+    }
+
+    DataDescription2& addParticles(list<ParticleDescription2> const& value)
+    {
+        particles.insert(particles.end(), value.begin(), value.end());
+        return *this;
+    }
+    DataDescription2& addParticle(ParticleDescription2 const& value)
+    {
+        addParticles({value});
+        return *this;
+    }
+    void clear()
+    {
+        clusters.clear();
+        particles.clear();
+    }
+    bool isEmpty() const
+    {
+        if (!clusters.empty()) {
+            return false;
+        }
+        if (!particles.empty()) {
+            return false;
+        }
+        return true;
+    }
+    RealVector2D calcCenter() const;
+    void shift(RealVector2D const& delta);
 };
+
 
 struct DescriptionNavigator
 {
-	unordered_set<uint64_t> cellIds;
-	unordered_set<uint64_t> particleIds;
-	map<uint64_t, uint64_t> clusterIdsByCellIds;
-	map<uint64_t, int> clusterIndicesByClusterIds;
-	map<uint64_t, int> clusterIndicesByCellIds;
-	map<uint64_t, int> cellIndicesByCellIds;
-	map<uint64_t, int> particleIndicesByParticleIds;
+	std::unordered_set<uint64_t> cellIds;
+    std::unordered_set<uint64_t> particleIds;
+    std::map<uint64_t, uint64_t> clusterIdsByCellIds;
+    std::map<uint64_t, int> clusterIndicesByClusterIds;
+    std::map<uint64_t, int> clusterIndicesByCellIds;
+    std::map<uint64_t, int> cellIndicesByCellIds;
+    std::map<uint64_t, int> particleIndicesByParticleIds;
 
-	void update(DataDescription const& data)
+	void update(DataDescription2 const& data)
 	{
 		cellIds.clear();
 		particleIds.clear();
@@ -307,47 +522,34 @@ struct DescriptionNavigator
 		particleIndicesByParticleIds.clear();
 
 		int clusterIndex = 0;
-		if (data.clusters) {
-			for (auto const &cluster : *data.clusters) {
-				clusterIndicesByClusterIds.insert_or_assign(cluster.id, clusterIndex);
-				int cellIndex = 0;
-				if (cluster.cells) {
-					for (auto const &cell : *cluster.cells) {
-						clusterIdsByCellIds.insert_or_assign(cell.id, cluster.id);
-						clusterIndicesByCellIds.insert_or_assign(cell.id, clusterIndex);
-						cellIndicesByCellIds.insert_or_assign(cell.id, cellIndex);
-						cellIds.insert(cell.id);
-						++cellIndex;
-					}
-				}
-				++clusterIndex;
+		for (auto const &cluster : data.clusters) {
+			clusterIndicesByClusterIds.insert_or_assign(cluster.id, clusterIndex);
+			int cellIndex = 0;
+			for (auto const &cell : cluster.cells) {
+				clusterIdsByCellIds.insert_or_assign(cell.id, cluster.id);
+				clusterIndicesByCellIds.insert_or_assign(cell.id, clusterIndex);
+				cellIndicesByCellIds.insert_or_assign(cell.id, cellIndex);
+				cellIds.insert(cell.id);
+				++cellIndex;
 			}
+			++clusterIndex;
 		}
 
 		int particleIndex = 0;
-		if (data.particles) {
-			for (auto const &particle : *data.particles) {
-				particleIndicesByParticleIds.insert_or_assign(particle.id, particleIndex);
-				particleIds.insert(particle.id);
-				++particleIndex;
-			}
+		for (auto const &particle : data.particles) {
+			particleIndicesByParticleIds.insert_or_assign(particle.id, particleIndex);
+			particleIds.insert(particle.id);
+			++particleIndex;
 		}
 	}
 };
 
-//DEPRECATED
-struct DEPRECATED_CellDescription
+//TEMP
+struct Converter
 {
-    uint64_t id = 0;
-
-    boost::optional<RealVector2D> pos;
-    boost::optional<double> energy;
-    boost::optional<int> maxConnections;
-    boost::optional<list<uint64_t>> connections;
-    boost::optional<bool> tokenBlocked;
-    boost::optional<int> tokenBranchNumber;
-    boost::optional<CellMetadata> metadata;
-    boost::optional<CellFeatureDescription> cellFeature;
-    boost::optional<vector<TokenDescription>> tokens;
-    boost::optional<int> tokenUsages;
+    static TokenDescription2 convert2(TokenDescription const& token);
+    static CellDescription2 convert2(CellDescription const& cell);
+    static ClusterDescription2 convert2(ClusterDescription const& cluster);
+    static ParticleDescription2 convert2(ParticleDescription const& particle);
+    static DataDescription2 convert2(DataDescription const& data);
 };
