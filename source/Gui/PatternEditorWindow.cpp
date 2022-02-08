98f542c42ed21b2396529bb95b3e21ad987426f4
Christian Heinemann <heinemann.christian@gmail.com>
2022-02-08 20:17:20 +0100

    tooltip extended

--- source/Gui/PatternEditorWindow.cpp
+++ source/Gui/PatternEditorWindow.cpp
@@ -261,7 +261,7 @@ void _PatternEditorWindow::processIntern()
     }
     ImGui::SameLine();
     AlienImGui::HelpMarker("If turned on, all changes made in this window or with the mouse cursor are applied to the cell clusters of the selected cell.\n"
-                           "If this option is disabled, the changes will be applied only to the selected cells.\n"
+                           "If this option is disabled, the changes will be applied only to the selected cells. In this case, cell connections with neighboring cells are recalculated.\n"
                            "If you hold down the SHIFT key, this toggle button is temporarily turned off.");
 
     _savePatternDialog->process();
