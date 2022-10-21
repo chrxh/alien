c23c8cc171229a19e60557fe7bf714c3c1c2ccb4
Christian Heinemann <heinemann.christian@gmail.com>
2022-10-21 10:14:39 +0200

    optimization for matrix multiplication

--- source/EngineGpuKernels/NeuralNetProcessor.cuh
+++ source/EngineGpuKernels/NeuralNetProcessor.cuh
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "sm_60_atomic_functions.h"
+
 #include "SimulationData.cuh"
 #include "CellFunctionProcessor.cuh"
 
@@ -29,8 +31,6 @@ __device__ __inline__ void NeuralNetProcessor::process(SimulationData& data, Sim
 
 __inline__ __device__ void NeuralNetProcessor::processCell(SimulationData& data, SimulationResult& result, Cell* cell)
 {
-    auto partition = calcPartition(MAX_CHANNELS, threadIdx.x, blockDim.x);
-
     __shared__ Activity outputActivity;
     __shared__ Activity inputActivity;
     if (0 == threadIdx.x) {
@@ -38,16 +38,28 @@ __inline__ __device__ void NeuralNetProcessor::processCell(SimulationData& data,
     }
     __syncthreads();
 
-    for (int i = partition.startIndex; i <= partition.endIndex; ++i) {
-        float sumInput = 0;
-        auto & neuronsState = cell->cellFunctionData.neuronFunction.neuronState;
-        for (int j = 0; j < MAX_CHANNELS; ++j) {
-            sumInput += neuronsState->weights[i][j] * inputActivity.channels[j];
-        }
-        sumInput += neuronsState->bias[i];
-        outputActivity.channels[i] = sigmoid(sumInput);
+    __shared__ float sumInput[MAX_CHANNELS];
+    auto channelPartition = calcPartition(MAX_CHANNELS, threadIdx.x, blockDim.x);
+    for (int i = channelPartition.startIndex; i <= channelPartition.endIndex; ++i) {
+        sumInput[i] = cell->cellFunctionData.neuronFunction.neuronState->bias[i];
     }
     __syncthreads();
+
+    auto matrixPartition = calcPartition(MAX_CHANNELS * MAX_CHANNELS, threadIdx.x, blockDim.x);
+    for (int c = matrixPartition.startIndex; c <= matrixPartition.endIndex; ++c) {
+        auto& neuronsState = cell->cellFunctionData.neuronFunction.neuronState;
+
+        auto i = c / MAX_CHANNELS;
+        auto j = c % MAX_CHANNELS;
+        atomicAdd_block(&sumInput[i], neuronsState->weights[i][j] * inputActivity.channels[j]);
+    }
+    __syncthreads();
+
+    for (int i = channelPartition.startIndex; i <= channelPartition.endIndex; ++i) {
+        outputActivity.channels[i] = sigmoid(sumInput[i]);
+    }
+    __syncthreads();
+    
 
     if (0 == threadIdx.x) {
         CellFunctionProcessor::setActivity(cell, outputActivity);
