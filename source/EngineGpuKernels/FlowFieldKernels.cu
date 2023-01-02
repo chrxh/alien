a0e3b8b2312e3104bf141151fb84a69d7e7036e3
Christian Heinemann <heinemann.christian@gmail.com>
2023-01-02 10:39:40 +0100

    unused code removed

--- source/EngineGpuKernels/FlowFieldKernels.cu
+++ source/EngineGpuKernels/FlowFieldKernels.cu
@@ -7,34 +7,6 @@
 
 namespace
 {
-    //__device__ float getHeight(float2 const& pos, BaseMap const& mapInfo)
-    //{
-    //    float result = 0;
-    //    for (int i = 0; i < cudaSimulationParameters.numFlowCenters; ++i) {
-    //        auto& radialFlow = cudaSimulationParameters.flowCenters[i];
-    //        auto dist = mapInfo.getDistance(pos, float2{radialFlow.posX, radialFlow.posY});
-    //        if (dist > radialFlow.radius) {
-    //            dist = radialFlow.radius;
-    //        }
-    //        if (Orientation::Clockwise == radialFlow.orientation) {
-    //            result += sqrtf(dist) * radialFlow.strength;
-    //        } else {
-    //            result -= sqrtf(dist) * radialFlow.strength;
-    //        }
-    //    }
-    //    return result;
-    //}
-
-    //__device__ float2 calcVelocity(float2 const& pos, BaseMap const& mapInfo)
-    //{
-    //    auto baseValue = getHeight(pos, mapInfo);
-    //    auto downValue = getHeight(pos + float2{0, 1}, mapInfo);
-    //    auto rightValue = getHeight(pos + float2{1, 0}, mapInfo);
-    //    float2 result{rightValue - baseValue, downValue - baseValue};
-    //    Math::rotateQuarterClockwise(result);
-    //    return result;
-    //}
-
     __device__ float getHeight(BaseMap const& map, float2 const& pos, int const& spotIndex)
     {
         auto const& spot = cudaSimulationParameters.spots[spotIndex];
