cba45e08f19f8d7c21ff981c8ae84185d010be5d
Christian Heinemann <heinemann.christian@gmail.com>
2023-02-28 21:45:46 +0100

    alpha.5

--- source/Base/Resources.h
+++ source/Base/Resources.h
@@ -2,7 +2,7 @@
 
 namespace Const
 {
-    std::string const ProgramVersion = "4.0.0.alpha.4";
+    std::string const ProgramVersion = "4.0.0.alpha.5";
 
     std::string const BasePath = "resources/";
 
