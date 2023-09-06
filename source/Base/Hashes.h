61cc52325f9a98587ece0c43a047cd01dc8ddbcf
Christian Heinemann <heinemann.christian@gmail.com>
2023-09-07 00:11:06 +0200

    compile error fixed

--- source/Base/Hashes.h
+++ source/Base/Hashes.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <tuple>
+#include <bit>
 
 namespace std
 {
-    template <typename A, typename B>
+    template <class A, class B>
     struct hash<pair<A, B>>
     {
-        size_t operator()(const pair<A, B>& p) const { return std::rotl(hash<A>{}(p.first), 1) ^ hash<B>{}(p.second); }
+        size_t operator()(pair<A, B> const& p) const { return std::rotl(hash<A>{}(p.first), 1) ^ hash<B>{}(p.second); }
     };
 }
 
