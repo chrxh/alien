8b7c641df5f35ec7016d9ccd57db8061345b5357
Christian Heinemann <heinemann.christian@gmail.com>
2023-09-28 21:34:21 +0200

    Revert "unused includes removed"
    This reverts commit d325ea917c0d0a94f1b146a623d64fcbcca0214c.

--- source/Gui/Main.cpp
+++ source/Gui/Main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <algorithm>
 #include <cstring>
 
 #include "Base/GlobalSettings.h"
 #include "Base/LoggingService.h"
+#include "Base/Resources.h"
 #include "Base/FileLogger.h"
 #include "EngineInterface/Serializer.h"
 #include "EngineImpl/SimulationControllerImpl.h"
