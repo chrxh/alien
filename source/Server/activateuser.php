811092339c312d176e0b7218f1d6eb0d53944e01
Christian Heinemann <heinemann.christian@gmail.com>
2023-10-29 08:54:13 +0100

    send Discord message if new user is activated

--- source/Server/activateuser.php
+++ source/Server/activateuser.php
@@ -17,10 +17,28 @@
     $success = false;
     if ($db->query("UPDATE user SET ACTIVATION_CODE='' where NAME='" . addslashes($userName) . "'")) {
         $success = true;
-
+        $discordPayload = createNewUserMessage($userName);
+        sendDiscordMessage($discordPayload);
     }
     echo json_encode(["result"=>$success]);
 
     $db->commit();
     $db->close();
+
+    // function for Discord messages
+    function createNewUserMessage($userName) {
+            return json_encode([
+                "username" => "alien-project",
+                "avatar_url" => "https://alien-project.org/alien-server/logo.png",
+                "content" => "New simulator added to the database",
+                "embeds" => [
+                    [
+                        "author" => [
+                            "name" => $userName,
+                            "icon_url" => "https://alien-project.org/alien-server/user.png"
+                        ],
+                    ]
+                ]
+            ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        }
 ?>
