860aa7d52dd608dde0fb9c2cd5b868254580011f
Christian Heinemann <heinemann.christian@gmail.com>
2023-12-31 12:06:08 +0100

    restrict energy particle merging to certain energy value

--- source/EngineGpuKernels/ParticleProcessor.cuh
+++ source/EngineGpuKernels/ParticleProcessor.cuh
@@ -60,12 +60,15 @@ __inline__ __device__ void ParticleProcessor::collision(SimulationData& data)
             if (lock.tryLock()) {
 
                 if (particle->energy > NEAR_ZERO && otherParticle->energy > NEAR_ZERO) {
-                    auto factor1 = particle->energy / (particle->energy + otherParticle->energy);
-                    otherParticle->vel = particle->vel * factor1 + otherParticle->vel * (1.0f - factor1);
-                    otherParticle->energy += particle->energy;
-                    otherParticle->lastAbsorbedCell = nullptr;
-                    particle->energy = 0;
-                    particle = nullptr;
+                    if (cudaSimulationParameters.particleTransformationAllowed
+                        || particle->energy + otherParticle->energy < cudaSimulationParameters.cellNormalEnergy[particle->color]) {
+                        auto factor1 = particle->energy / (particle->energy + otherParticle->energy);
+                        otherParticle->vel = particle->vel * factor1 + otherParticle->vel * (1.0f - factor1);
+                        otherParticle->energy += particle->energy;
+                        otherParticle->lastAbsorbedCell = nullptr;
+                        particle->energy = 0;
+                        particle = nullptr;
+                    }
                 }
 
                 lock.releaseLock();
