d299dda2232de1404faedc9a52c4fe8ce1fabce0
Christian Heinemann <heinemann.christian@gmail.com>
2024-02-12 19:09:50 +0100

    allow upload large simulation via chunking: server script

---
+++ scripts/Server/appendsimulationdata.php
@@ -0,0 +1,47 @@
+<?php
+    require './helpers.php';
+    require './hooks.php';
+
+    $db = connectToDB();
+    $db->begin_transaction();
+
+    $userName = $_POST["userName"];
+    $pw = $_POST["password"];
+
+    if (!checkPw($db, $userName, $pw)) {
+        echo json_encode(["result"=>false]);
+        $db->close();
+        exit;
+    }
+
+    $userObj = $db->query("SELECT u.ID as id FROM user u WHERE u.NAME='".addslashes($userName)."'")->fetch_object();
+    if (!$userObj) {
+        echo json_encode(["result"=>false]);
+        $db->close();
+        exit;
+    }
+
+    $simId = $_POST['simId'];
+    $content = $_POST['content'];
+    $size = strlen($content);
+
+    $obj = $db->query("SELECT sim.USER_ID as userId, sim.SIZE as size FROM simulation sim WHERE sim.ID=$simId")->fetch_object();
+    if (!$obj || strcmp($userObj->id, $obj->userId) != 0 ) {
+        echo json_encode(["result"=>false]);
+        $db->close();
+        exit;
+    }
+
+    $newSize = (int)$obj->size + $size;
+
+    if (!$db->query("UPDATE simulation SET content = CONCAT(content,'" . addslashes($content) . "'), size = $newSize WHERE ID = $simId")) {
+        echo json_encode(["result"=>false]);
+        $db->close();
+        exit;
+    }
+
+    echo json_encode(["result"=>true]);
+
+    $db->commit();
+    $db->close();
+?>
