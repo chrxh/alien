2e87ac4349972ccbd10fa7e97dd3ac4cddd7bbaa
Christian Heinemann <heinemann.christian@gmail.com>
2024-10-19 14:50:10 +0200

    thread safe singleton

--- source/Base/Singleton.h
+++ source/Base/Singleton.h
@@ -1,32 +1,43 @@
 #pragma once
 
+#include <memory>
+#include <mutex>
+
 #define MAKE_SINGLETON(ClassName) \
 public: \
     static ClassName& get() \
     { \
-        static ClassName instance; \
-        return instance; \
+        static std::unique_ptr<ClassName> instance; \
+        static std::mutex mutex; \
+        if (!instance) { \
+            std::lock_guard<std::mutex> lock(mutex); \
+            instance = std::unique_ptr<ClassName>(new ClassName); \
+        } \
+        return *instance.get(); \
     } \
 \
 private: \
     ClassName() = default; \
-    ~ClassName() = default; \
     ClassName(ClassName const&) = delete; \
-    ClassName& operator=(ClassName const&) = delete; \
     ClassName(ClassName&&) = delete; \
+    ClassName& operator=(ClassName const&) = delete; \
     ClassName& operator=(ClassName&&) = delete
 
 #define MAKE_SINGLETON_CUSTOMIZED(ClassName) \
 public: \
     static ClassName& get() \
     { \
-        static ClassName instance; \
-        return instance; \
+        static std::unique_ptr<ClassName> instance; \
+        static std::mutex mutex; \
+        if (!instance) { \
+            std::lock_guard<std::mutex> lock(mutex); \
+            instance = std::unique_ptr<ClassName>(new ClassName); \
+        } \
+        return *instance.get(); \
     } \
 \
 private: \
-    ~ClassName() = default; \
     ClassName(ClassName const&) = delete; \
-    ClassName& operator=(ClassName const&) = delete; \
     ClassName(ClassName&&) = delete; \
+    ClassName& operator=(ClassName const&) = delete; \
     ClassName& operator=(ClassName&&) = delete
