a3f5b4b5e552c41f22cb70246ec4c104311c7220
Christian Heinemann <heinemann.christian@gmail.com>
2025-05-15 20:49:07 +0200

    missing files

---
+++ source/EngineInterface/ArraySizesForObjectTOs.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdint.h>
+
+struct ArraySizesForObjectTOs
+{
+    uint64_t cellArraySize = 0;
+    uint64_t particleArraySize = 0;
+    uint64_t heapSize = 0;
+};
---
+++ source/EngineInterface/ArraySizesForObjects.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdint.h>
+
+struct ArraySizesForObjects
+{
+    uint64_t cellArraySize = 0;
+    uint64_t particleArraySize = 0;
+    uint64_t heapSize = 0;
+};
