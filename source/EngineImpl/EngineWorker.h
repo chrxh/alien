73f8b566a60b6ffdf6997bb28769d06db2fdca77
Christian Heinemann <heinemann.christian@gmail.com>
2025-09-06 15:34:23 +0200

    Prevent crash in sync mode

--- source/EngineImpl/EngineWorker.h
+++ source/EngineImpl/EngineWorker.h
@@ -142,7 +142,7 @@ private:
     ExceptionData _exceptionData;
 
     //async jobs
-    std::mutex _mutexForEngineWorkerGuard;
+    std::recursive_mutex _mutexForEngineWorkerGuard;
     mutable std::mutex _mutexForAsyncJobs;
     std::optional<GpuSettings> _updateGpuSettingsJob;
 
