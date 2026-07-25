eced91f405034056adfe5a0a3c1654f258027f25
Christian Heinemann <heinemann.christian@gmail.com>
2026-07-25 16:56:03 +0200

    Labels corrected

--- source/Gui/InspectionWindow.cpp
+++ source/Gui/InspectionWindow.cpp
@@ -581,8 +581,8 @@ void _InspectionWindow::processCreatureProperties(ExtendedObjectDesc& extendedOb
     inspectorText("Generation", std::to_string(creature._generation));
     inspectorText("Num cells", std::to_string(creature._numCells));
     AlienGui::InputInt(AlienGui::InputIntParameters().name("Lineage id").textWidth(TextWidth), creature._lineageId);
-    inspectorText("Mutations (lineage)", std::to_string(creature._accumulatedMutationsInLineage));
-    inspectorText("Mutations (total)", std::to_string(creature._accumulatedMutations));
+    inspectorText("Mutations (total)", std::to_string(creature._accumulatedMutationsInLineage));
+    inspectorText("Mutations (in lineage)", std::to_string(creature._accumulatedMutations));
     auto& genome = extendedObject.genome.value();
     inspectorText("Genome name", genome._name);
     inspectorText("Resistance to injection", genome._resistanceToInjection ? "Yes" : "No");
